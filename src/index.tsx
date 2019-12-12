bea788ae83dc33b92e5a7dc9b53fff7c6df68d05
Paul <venard.paul@gmail.com>
2019-12-12 01:08:20 +0100

    feat(authentification): start login page

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
+import {
+    BrowserRouter as Router,
+    Route,
+    Switch
+} from 'react-router-dom';
 import { initializeAxios } from './data/requestFactory';
+import Login from "./pages/auth";
 
 initializeAxios();
 
 const App: React.FC = () => {
     return (
-        <p>Hello world !</p>
+       <Router>
+           <Switch>
+               <Route path="/login" component={Login} />
+           </Switch>
+       </Router>
     );
 };
 ReactDOM.render(<App />, document.getElementById('root'));
