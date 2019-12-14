ea9612daeb40a4b4f7ac4e5d74fcbe79dc5cf790
Paul <venard.paul@gmail.com>
2019-12-14 15:54:23 +0100

    feat(global): set up main router

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
+import './i18n';
 import {
     BrowserRouter as Router,
     Route,
-    Switch
+    Switch,
+    Redirect
 } from 'react-router-dom';
 import { initializeAxios } from './data/requestFactory';
-import Login from "./pages/auth";
+import Login from "./pages/security";
 
 initializeAxios();
 
@@ -15,10 +17,13 @@ const App: React.FC = () => {
     return (
        <Router>
            <Switch>
+               <Redirect path="/" exact to="/login" />
                <Route path="/login" component={Login} />
+               <Route path="/" component={Login} />
            </Switch>
        </Router>
     );
 };
+
 ReactDOM.render(<App />, document.getElementById('root'));
 serviceWorker.register();
