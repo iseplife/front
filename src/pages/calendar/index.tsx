f3223391ba8898983c7577832ad0a5fb53b34d4d
Paul <venard.paul@gmail.com>
2019-12-18 19:24:06 +0100

    feat(event): add calendar dependencies

--- src/pages/calendar/index.tsx
+++ src/pages/calendar/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import EventsScroller from "../../components/Event/EventsScroller";
-import Calendar from "../../components/Event/Calendar";
+import Calendar from 'react-calendar'
 
 const Events: React.FC = () => {
     return (
         <div id="events-page" className="flex px-4 flex-row">
             <EventsScroller className="md:w-3/4 w-full" />
             <div className="mt-5 md:w-1/4 w-1 md:block hidden">
-               <Calendar className="sticky" style={{top: "1.25rem"}}/>
+               <Calendar className="side-calendar"/>
             </div>
         </div>
     )
