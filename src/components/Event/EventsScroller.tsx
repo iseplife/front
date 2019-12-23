9002e8a2eb1d7097104d63fc2d0ad4fe49147945
Paul <venard.paul@gmail.com>
2019-12-23 01:14:11 +0100

    feat(events): avoid un-async useReducter

--- src/components/Event/EventsScroller.tsx
+++ src/components/Event/EventsScroller.tsx
@@ -5,9 +5,8 @@ import {getEventsAround, getNextEvents, getPreviousEvents} from "../../data/even
 import React, {useEffect, useReducer} from "react";
 import Loading from "../Loading";
 import Event from "../Event"
-import {AxiosPromise} from "axios";
 
-const PIXEL_BEFORE_REACHED = 100;
+const PIXEL_BEFORE_REACHED = 50;
 const INITIAL_LOADER: Loader = {loading: false, count: 0, over: false};
 const INITIAL_STATE: EventScrollerState = {
     events: {},
@@ -32,10 +31,8 @@ type ActionType =
     | "FETCH_AROUND_COMPLETE"
     | "FETCH_UP_INIT"
     | "FETCH_UP_COMPLETE"
-    | "FETCH_UP_OVER"
     | "FETCH_DOWN_INIT"
-    | "FETCH_DOWN_COMPLETE"
-    | "FETCH_DOWN_OVER";
+    | "FETCH_DOWN_COMPLETE";
 
 type ReducerAction = {
     type: ActionType,
@@ -51,41 +48,29 @@ type EventScrollerState = {
 }
 
 const reducer: React.Reducer<EventScrollerState, ReducerAction> = (state, action): EventScrollerState => {
-    const fetchEvents = async (f: (...param: any) => AxiosPromise<EventList>, ...param: any): Promise<EventList> => {
-        const res = await f(...param);
-        return res.data;
-    };
-    let res;
     switch (action.type) {
         case "FETCH_AROUND_INIT":
             return ({...state, loading: true});
         case "FETCH_AROUND_COMPLETE":
             return ({...state, loading: false, events: action.events || {}});
         case "FETCH_UP_INIT":
-            return ({...state, up: {...state.up, loading: true} });
+            return ({...state, up: {...state.up, loading: true}});
         case "FETCH_UP_COMPLETE":
-            const nextDay = new Date(Number(Object.keys(state.events).pop()));
-            res = fetchEvents(getNextEvents, nextDay, state.up.count);
             return ({
                 ...state,
-                //events: {...state.events, ...res},
-                up: {over: Object.keys(res) && true, loading: false, count: ++state.up.count}
+                events: {...state.events, ...action.events},
+                up: {over: Object.keys(action.events || {}) && true, loading: false, count: ++state.up.count}
 
             });
         case "FETCH_DOWN_INIT":
             return ({...state, down: {...state.down, loading: true}});
         case "FETCH_DOWN_COMPLETE":
-            const previousDay = new Date(Number(Object.keys(state.events).shift()));
-            res = fetchEvents(getPreviousEvents, previousDay, state.down.count).then(r => console.log(r));
             return ({
                 ...state,
-                //events: {...state.events, ...res},
-                down: {over: Object.keys(res) && true, loading: false, count: ++state.down.count, }
+                events: {...state.events, ...action.events},
+                down: {over: Object.keys(action.events || {}) && true, loading: false, count: ++state.down.count,}
             });
-        case "FETCH_DOWN_OVER":
-            break;
     }
-    return state;
 };
 
 const EventsScroller: React.FC<EventsScrollerProps> = ({className, timestamp = Date.now()}) => {
@@ -98,7 +83,9 @@ const EventsScroller: React.FC<EventsScrollerProps> = ({className, timestamp = D
             const res = await getEventsAround(new Date(timestamp));
             dispatch({type: 'FETCH_AROUND_COMPLETE', events: res.data});
         };
-        fetchEvents()
+        fetchEvents().then(
+
+        )
     }, [timestamp]);
 
     useEffect(() => {
@@ -119,18 +106,31 @@ const EventsScroller: React.FC<EventsScrollerProps> = ({className, timestamp = D
     }, []);
 
     useEffect(() => {
-        if (up.loading) {
-            dispatch({type: "FETCH_UP_COMPLETE"});
+        if (!up.over && up.loading) {
+            const fetchEvents = async () => {
+                const previousDay = new Date(Number(Object.keys(events).pop()));
+                const res = await getNextEvents(previousDay, up.count);
+                dispatch({type: 'FETCH_UP_COMPLETE', events: res.data});
+            };
+            fetchEvents()
         }
-        if (down.loading) {
-            dispatch({type: "FETCH_DOWN_COMPLETE"});
+    }, [up.loading]);
+
+    useEffect(() => {
+        if (!down.over && down.loading) {
+            const fetchEvents = async () => {
+                const previousDay = new Date(Number(Object.keys(events).shift()));
+                const res = await getPreviousEvents(previousDay, up.count);
+                dispatch({type: 'FETCH_DOWN_COMPLETE', events: res.data});
+            };
+            fetchEvents()
         }
-    }, [up.loading, down.loading]);
+    }, [down.loading]);
 
     return (
         <div id="events-list" className={`min-h-screen h-auto ${className}`}>
             <div className="h-12 mb-3">
-                {up.loading && <Loading size="3x"/>}
+                {!up.over && up.loading && <Loading size="3x"/>}
             </div>
             {Object.entries(events).map(([timestamp, events]) => {
                     const date: Date = toDate(Number(timestamp));
@@ -166,7 +166,7 @@ const EventsScroller: React.FC<EventsScrollerProps> = ({className, timestamp = D
                 }
             )}
             <div className="h-12 mb-3">
-                {down.loading && <Loading size="3x"/>}
+                {!down.over && down.loading && <Loading size="3x"/>}
             </div>
         </div>
     )
