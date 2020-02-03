5e8761cee867f4cb39032f7684454d3c7f9c4eb3
Paul <venard.paul@gmail.com>
2020-02-03 10:42:45 +0100

    feat(i18): fix minor translation + create languages const

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,6 +2,8 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import translations from "./translations/translations";
 
+export const SUPPORTED_LANGUAGES = Object.keys(translations);
+
 i18n
     .use(initReactI18next) // passes i18n down to react-i18next
     .init({
