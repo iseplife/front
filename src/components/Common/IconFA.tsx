ca9ce0bfab22943327e3276feaef19ce312c5e79
Bastien <basgrignon@gmail.com>
2020-02-19 00:22:03 +0100

    Make font awesome icon component works and more generic

--- src/components/Common/IconFA.tsx
+++ src/components/Common/IconFA.tsx
@@ -5,12 +5,13 @@ export type IconFAProps = {
     spin?: boolean
     size?: "xs" | "sm" | "lg" | "2x" | "3x" | "4x" | "5x" | "6x" | "7x" | "8x" | "9x" | "10x"
     style?: "solid" | "regular" | "light" | "duotone" | "brands"
+    className?: string
 }
 
 
-export const IconFA: React.FC<IconFAProps> = ({name, spin, size, style}) => {
+export const IconFA: React.FC<IconFAProps> = ({name, spin, size, style, className}) => {
     return (
-        <i className={`text-indigo-200 far ${"fa" + style?.charAt(0)} ${spin ? "fa-spin" : ""} ${name} fa-${size}`}/>
+        <i className={`${className} ${"fa" + style?.charAt(0)} ${spin ? "fa-spin" : ""} ${name} fa-${size}`}/>
     );
 };
 IconFA.defaultProps = {
