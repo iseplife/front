1a99583a7f74461ebb8634c9813fc9eb87e6daf8
Bastien <basgrignon@gmail.com>
2020-02-19 11:24:17 +0000

    Update to avoid undefined variable, add empty string

--- src/components/Common/IconFA.tsx
+++ src/components/Common/IconFA.tsx
@@ -1,1 +1,23 @@
-Šjh®Ô^iË_®‰«y§-{h®Ûr¥â¢q@>º)²v¦zËkŠx,¦)Û¢‰^j{"Íìl²i`Ûñãqëñóq×Ll·)^²‰bv· ºV«–(!µÛ¨¶‰Şn¶§vÇ%jËjg¬¶¸§ìi¢»\¢{-!Ê'^iËE‡(œP®Šl©²˜§²,Ş²Ü¥yÉZ²ÃZ™
+import React from "react";
+
+export type IconFAProps = {
+    name: string;
+    spin?: boolean
+    size?: "xs" | "sm" | "lg" | "2x" | "3x" | "4x" | "5x" | "6x" | "7x" | "8x" | "9x" | "10x"
+    style?: "solid" | "regular" | "light" | "duotone" | "brands"
+    className?: string
+}
+
+
+export const IconFA: React.FC<IconFAProps> = ({name, spin, size, style}) => {
+export const IconFA: React.FC<IconFAProps> = ({name, spin, size, style, className = ""}) => {
+    return (
+        <i className={`text-indigo-200 far ${"fa" + style?.charAt(0)} ${spin ? "fa-spin" : ""} ${name} fa-${size}`}/>
+        <i className={`${className} ${"fa" + style?.charAt(0)} ${spin ? "fa-spin" : ""} ${name} fa-${size}`}/>
+    );
+};
+IconFA.defaultProps = {
+    spin: false,
+    size: "sm",
+    style: "regular"
+};
