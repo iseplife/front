a0601a701af71d394fe9e95cec9d23c2f79e555a
Paul <venard.paul@gmail.com>
2020-02-23 11:52:16 +0100

    feat(typo): adapt api url env var name

--- src/data/requestFactory.ts
+++ src/data/requestFactory.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import {getCookie} from "./security/cookie";
 
-const url = process.env.REACT_APP_BACKEND_URL || "localhost:8080";
+const url = process.env.REACT_APP_API_URL || "localhost:8080";
 const httpProtocol = process.env.REACT_APP_HTTP_PROTOCOL || "http";
 const wsProtocol = process.env.REACT_APP_WS_PROTOCOL || "ws";
 
