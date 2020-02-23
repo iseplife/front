38a1d626d7faea3f7ab1af2fdbcf2da272a3adeb
Paul <venard.paul@gmail.com>
2020-02-23 22:13:24 +0100

    feat(interceptor): handle errors

--- src/components/Template/Interceptor.tsx
+++ src/components/Template/Interceptor.tsx
@@ -2,6 +2,7 @@ import React, {useEffect} from "react";
 import {withRouter, RouteComponentProps} from "react-router";
 import axios, {AxiosError, AxiosResponse} from 'axios';
 import {removeTokens, setTokens} from "../../data/security";
+import {message} from "antd/es";
 
 
 const axiosResponseInterceptor = (response: AxiosResponse) => {
@@ -17,24 +18,25 @@ const axiosResponseInterceptor = (response: AxiosResponse) => {
 };
 
 const axiosErrorInterceptor = (props: RouteComponentProps) => (error: AxiosError) => {
-    props.history.push('/404');
     if (error.response) {
         switch (error.response.status) {
             case 404:
                 props.history.push('/404');
                 break;
-
             case 401:
             case 403:
                 removeTokens();
                 props.history.push('/login');
+                message.error('Vous avez été déconnecté !');
                 break;
 
             case 500:
             case 400:
+                message.error('Un probleme a été rencontré');
+                console.error( `[${error.code}] ${error.message}`);
                 break;
             case 503:
-
+                message.error('Serveur indisponible');
                 break;
 
             default:
