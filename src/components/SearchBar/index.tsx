925c2564ec8315dd3878e34feb0a7385ee59bb36
Bastien <basgrignon@gmail.com>
2020-03-23 21:32:00 +0100

    feat(searchBar): First implementation of the searchBar - Fetch remote data (currently club search only) - Custom dropdown with filter

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,36 +1,17 @@
-import {Icon, Input, Select, Spin} from "antd";
+import {Checkbox, Divider, Input, Select, Spin} from "antd";
 import React, {useEffect, useState} from "react";
-import {getFakeData, globalSearch, searchClub} from "../../data/searchbar";
+import * as querystring from "querystring";
+import {searchClub} from "../../data/searchbar";
+import CheckboxGroup from "antd/es/checkbox/Group";
 
 const {Option, OptGroup} = Select;
-const {Search} = Input;
 
-const fakeData = [
-    {
-        id: 1,
-        type: "CLUB",
-        name: "Air ISEP",
-        thumbUrl: "/tmp/picture/test.jpg"
-    },
-    {
-        id: 2,
-        type: "CLUB",
-        name: "Garage ISEP",
-        thumbUrl: "/tmp/picture/garage.jpg"
-    },
-    {
-        id: 3,
-        type: "STUDENT",
-        name: "Bastien Grignon",
-        thumbUrl: "/tmp/picture/garage.jpg"
-    },
-    {
-        id: 4,
-        type: "STUDENT",
-        name: "Lucas Perrault",
-        thumbUrl: "/tmp/picture/garage.jpg"
-    }
-];
+interface SelectInputProps {
+    id?: number
+    type?: string
+    text: string
+    value: string
+}
 
 type SearchItem = {
     id: number,
@@ -42,38 +23,116 @@ type SearchItem = {
 }
 
 const SearchBar: any = () => {
+    const [data, setData] = useState<SelectInputProps[]>([]);
+    const [value, setValue] = useState<any>(undefined);
+    const [fetching, setFetching] = useState<boolean>(false);
+    let currentValue: string;
 
-    const [isLoading, setIsLoading] = useState<boolean>(true);
-    const [currentSearch, setCurrentSearch] = useState<string>("");
-    const [data, setData] = useState<any>(fakeData);
+    useEffect(() => {
+        updateSearchItems("");
+    }, []);
 
-    const handleSearch = (typedValue: string) => {
-        console.log("Nouvelle recherche : " + typedValue);
-        setData(null);
-        setIsLoading(true);
-        setData(fakeData);
-        setIsLoading(false);
-
+    const groupBy = (array: SelectInputProps[], key: string) => {
+        return array.reduce((result: any, currentValue: any) => {
+            (result[currentValue[key]] = result[currentValue[key]] || []).push(currentValue);
+            return result;
+        }, {});
     };
 
-    const handleChange = (typedValue: string) => {
-        setData(null);
-        setCurrentSearch(typedValue);
-        setIsLoading(false)
+    const updateSearchItems = (queryParams: string) => {
+        searchClub(queryParams).then(res => {
+            // You need to create the SearchItem type
+            const searchItems: SearchItem[] = res.data;
+
+            const data: SelectInputProps[] = [];
+            searchItems.map((s: SearchItem) => {
+                data.push({
+                    id: s.id,
+                    type: s.type,
+                    value: s.name,
+                    text: s.name
+                });
+            });
+            setData(data);
+        }).catch((e) => e)
+            .finally(() => setFetching(false));
+    };
+
+    const fetch = (value: string) => {
+        currentValue = value;
+        setFetching(true);
+        if (currentValue === value) {
+            updateSearchItems(value);
+        }
+    };
+
+    const handleSearch = (currentValue: string) => {
+        if (!!currentValue) {
+            setValue(currentValue);
+            fetch(currentValue);
+        } else {
+            updateSearchItems("");
+            setValue("");
+        }
+    };
+
+    const handleChange = () => {
+        setValue(undefined);
+    };
+
+    const handleSelect = (value: string) => {
+        const arrayOfTypeAndId = value.split("/");
+        const id = arrayOfTypeAndId[0];
+        const type = arrayOfTypeAndId[1];
+        console.log(value, id, type);
+        setValue(value);
+    };
+
+    const renderOptions = () => {
+        const selectGroupByJson = (groupBy(data, "type"));
+        const keys = Object.keys(selectGroupByJson);
+
+        if (!!keys.length) {
+            return keys.map((key: string) =>
+                <OptGroup label={key} key={key}>
+                    {
+                        (selectGroupByJson[key] as SelectInputProps[])
+                            .map((s: SelectInputProps) => <Option key={s.value}
+                                                                  value={`${s.id}/${s.type}`}>{s.text}</Option>)
+                    }
+                </OptGroup>)
+        } else {
+            return "";
+        }
     };
 
     return (
-        <Select showSearch={true} onSearch={handleSearch} onChange={handleChange}
-                filterOption={false} suffixIcon={<Icon type="search"/>}
-                defaultActiveFirstOption={false} className="my-auto w-1/4"
-                placeholder="Cherchez ce que vous voulez">
-            {
-                data.map((item: any, key: any) => {
-                    return (
-                        <Option key{...key}>{item.name}</Option>
-                    );
-                })
-            }
+        <Select showSearch
+                mode="default"
+                value={value}
+                className="my-auto w-1/4"
+                defaultActiveFirstOption={false}
+                showArrow={false}
+                filterOption={false}
+                onSearch={handleSearch}
+                onChange={handleChange}
+                onSelect={handleSelect}
+                loading={fetching}
+                dropdownRender={menu => (
+                    <div>
+                        <div className="flex-no-wrap p-3">
+                            <div className="font-bold">Filtre :</div>
+                            <CheckboxGroup>
+                                <Checkbox>Elève</Checkbox>
+                                <Checkbox>Evenement</Checkbox>
+                                <Checkbox>Club</Checkbox>
+                            </CheckboxGroup>
+                        </div>
+                        <Divider style={{margin: '4px 0'}}/>
+                        {menu}
+                    </div>
+                )}>
+            {renderOptions()}
         </Select>
     );
 };
