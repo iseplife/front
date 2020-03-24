cdc7afe49357bbb01327f8102acd89e7f5908db3
Bastien <basgrignon@gmail.com>
2020-03-24 09:31:15 +0100

    feat(userDrawer): Add student request to API

--- src/data/student/index.ts
+++ src/data/student/index.ts
@@ -1,7 +1,14 @@
-import {AxiosPromise} from "axios";
+import axios, {AxiosPromise} from "axios";
 import {Student} from "./types";
-import axios from 'axios';
 
 export function getLoggedUser(): AxiosPromise<Student> {
     return axios.get('/student/me');
 }
+
+export const getStudentById = (studentId: number): AxiosPromise<Student> => {
+    return axios.get(`/student/${studentId}`);
+};
+
+export const getClubsForStudent = (studentId: number): AxiosPromise<any> => {
+    return axios.get(`/student/${studentId}/club`);
+};
