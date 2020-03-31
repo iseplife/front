02e88e06bc0b7074d2d5433ef24d4b5fd59d19aa
Perrault <lucas.perrault@hotmail.com>
2020-03-31 10:44:17 +0200

    type (GALLERY)

---
+++ src/data/gallery/type.tsx
@@ -0,0 +1,20 @@
+import {Club} from "../club/type";
+
+export type Gallery = {
+    id: number
+    name: string
+    official: true
+    club: Club,
+    creation: Date,
+    embedType: string,
+    coverImage: Media
+    previewImages: Media[]
+}
+
+export type Media = {
+    id: number
+    creation: Date
+    name: string
+    embedType: string
+    nsfw: string
+}
