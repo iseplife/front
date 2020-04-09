894ae62d2de6b6e50c02b55574c6caf3fd9bf255
Bastien <basgrignon@gmail.com>
2020-04-09 14:43:27 +0200

    feat(userDrawer): Remove `;` and `,` from types

--- src/data/club/types.ts
+++ src/data/club/types.ts
@@ -3,23 +3,23 @@ import {Student} from "../student/types";
 import RoleType from "../../constants/RoleType";
 
 export type Club = {
-    id: number,
-    name: string,
-    type: ClubType,
-    description: string,
-    logoUrl: string,
-    createdAt: number,
-    archivedAt?: number,
+    id: number
+    name: string
+    type: ClubType
+    description: string
+    logoUrl: string
+    createdAt: number
+    archivedAt?: number
 }
 
 export type ClubPreview = {
-    id: number,
-    name: string,
-    logoUrl: string,
+    id: number
+    name: string
+    logoUrl: string
 }
 
 export type ClubMemberView = {
-    club: Club,
-    role: RoleType,
+    club: Club
+    role: RoleType
     member: Student
 }
