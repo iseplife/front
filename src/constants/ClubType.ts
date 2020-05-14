08b4da99e2ba940239b463f9f188d784ced354b5
Paul Vénard <pvenard@juniorisep.com>
2020-05-14 15:47:10 +0200

    feat(club-admin): set up admin panel

--- src/constants/ClubType.ts
+++ src/constants/ClubType.ts
@@ -1,8 +1,8 @@
 enum ClubType {
-    "STUDENT_UNION" = 0,
-    "ART_UNION" = 1,
-    "SPORT_UNION" = 2,
-    "CLUB" = 3
+    STUDENT_UNION = "STUDENT_UNION",
+    ART_UNION = "ART_UNION",
+    SPORT_UNION = "SPORT_UNION",
+    CLUB ="CLUB"
 }
-
+export const ClubTypeArray = [ClubType.STUDENT_UNION, ClubType.ART_UNION, ClubType.SPORT_UNION, ClubType.CLUB];
 export default ClubType;
