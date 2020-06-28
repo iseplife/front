3ca4107d178352db0776dee0500efd0a43408cda
Bastien <basgrignon@gmail.com>
2020-06-28 19:44:26 +0200

    feat(searchBar): Add translations for searchBar

--- src/translations/translations.ts
+++ src/translations/translations.ts
@@ -4,6 +4,7 @@ import event_en from "./en/event.json";
 import common_en from "./en/common.json";
 import gallery_en from "./en/gallery.json";
 import user_en from "./en/user.json";
+import search_en from "./en/search.json";
 
 import login_fr from "./fr/login.json";
 import date_fr from "./fr/date.json";
@@ -11,6 +12,7 @@ import event_fr from "./fr/event.json";
 import common_fr from "./fr/common.json";
 import gallery_fr from "./fr/gallery.json";
 import user_fr from "./fr/user.json";
+import search_fr from "./fr/search.json";
 
 export const en = {
     login: login_en,
@@ -18,7 +20,8 @@ export const en = {
     common: common_en,
     event: event_en,
     gallery: gallery_en,
-    user: user_en
+    user: user_en,
+    search: search_en
 };
 export const fr = {
     login: login_fr,
@@ -26,7 +29,8 @@ export const fr = {
     common: common_fr,
     event: event_fr,
     gallery: gallery_fr,
-    user: user_fr
+    user: user_fr,
+    search: search_fr
 };
 
 const translations = {
