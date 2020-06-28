17178aa5f9c9cfab2cec6947533ef4ef8655e681
Bastien <basgrignon@gmail.com>
2020-06-28 20:19:49 +0200

    feat(searchBar): Update search queries to API

--- src/data/searchbar/index.ts
+++ src/data/searchbar/index.ts
@@ -1,13 +1,17 @@
 import axios, {AxiosPromise} from "axios";
 
-export const getFakeData = (): AxiosPromise<any> => {
-    return axios.get("https://jsonplaceholder.typicode.com/users");
+export const searchClub = (name: string, page: number): AxiosPromise<any> => {
+    return axios.get("/search/club", {params: {name: name, page: page}});
 };
 
-export const searchClub = (name: string, page: number): AxiosPromise<any> => {
-    return axios.get("/search/club", {params: {filter: name, page: page}});
+export const searchStudent = (name: string, promos: string, page: number): AxiosPromise<any> => {
+    return axios.get("/search/student", {params: {name: name, promos: promos, page: page}});
+};
+
+export const searchEvent = (name: string, page: number): AxiosPromise<any> => {
+    return axios.get("/search/event", {params: {name: name, page: page}});
 };
 
 export const globalSearch = (name: string, page: number): AxiosPromise<any> => {
-    return axios.get("/search", {params: {filter: name, page: page}});
+    return axios.get("/search", {params: {name: name, page: page}});
 };
