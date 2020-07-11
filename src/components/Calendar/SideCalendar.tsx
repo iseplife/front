b49076b22bbe727d3a28a9fe7320c54937ec9147
Paul Vénard <pvenard@juniorisep.com>
2020-07-11 19:51:45 +0200

    feat(calendar): start filter

--- src/components/Calendar/SideCalendar.tsx
+++ src/components/Calendar/SideCalendar.tsx
@@ -1,31 +1,56 @@
-import React, {useMemo} from "react";
-import Calendar from 'react-calendar';
-import {useTranslation} from "react-i18next";
+import React, {Dispatch} from "react"
+import Calendar from "react-calendar"
+import {useTranslation} from "react-i18next"
+import {EventFilter, FilterReducerAction} from "../../data/event/types"
+import Tag from "../Common/Tag"
 
 type SideCalendarProps = {
     date: Date
-    handleChange: (d: Date) => void
+    handleDate: (d: Date) => void
     className: string
+    filter: EventFilter,
+    handleFilter: Dispatch<FilterReducerAction>
 };
-const SideCalendar: React.FC<SideCalendarProps> = ({date, handleChange, className}) => {
-    const {i18n} = useTranslation("event");
+const SideCalendar: React.FC<SideCalendarProps> = ({filter, handleFilter, date, handleDate, className}) => {
+	const {t, i18n} = useTranslation("event")
 
+	return (
+		<div className={`${className} flex flex-column shadow bg-white p-4 relative`}>
+			<div className="sticky w-full" style={{top: "1rem"}}>
+				<Calendar
+					locale={i18n.language}
+					value={date}
+					onChange={(d) => handleDate(Array.isArray(d) ? d[0]: d)}
+				/>
+				<div className="mt-2 px-4">
+					<div id="feeds-filter">
+						{Object.entries(filter.feeds).map(([feed, selected]) => (
+							<Tag selected={selected} onClick={() => handleFilter({type: "TOGGLE_FEED", name: feed})}>
+								{feed}
+							</Tag>
+						))}
+					</div>
+					<p className="text-xs text-color text-gray-600 font-dinotcb uppercase mt-2">Types :</p>
+					<div id="types-filter" className="flex flex-wrap">
+						{Object.entries(filter.types).map(([type, visible]) => (
+							<div
+								key={type}
+								className=" m-1 px-2 py-1 text-xs text-indigo-500 font-dinot font-bold cursor-pointer">
+								<input className="rounded" type="checkbox"
+									onChange={() => handleFilter({type: "TOGGLE_TYPE", name: type})} checked={visible}
 
-
-    return (
-        <div className={`${className} flex flex-column shadow bg-white`}>
-            <Calendar
-                className=""
-                locale={i18n.language}
-
-                value={date}
-                onChange={(d) => handleChange(Array.isArray(d) ? d[0]: d)}
-            />
-        </div>
-    )
-};
+								/>
+								{t(`type.${type}`)}
+							</div>
+						))}
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+}
 
 SideCalendar.defaultProps = {
-    className: ""
+	className: ""
 }
-export default SideCalendar;
+export default SideCalendar
--- src/pages/default/calendar/index.tsx
+++ src/pages/default/calendar/index.tsx
@@ -1,6 +1,8 @@
-import React, {useCallback, useMemo, useState} from "react";
+import React, {useCallback, useMemo, useReducer, useState} from "react";
 import {Calendar, dateFnsLocalizer, View} from 'react-big-calendar'
-import { Button, Radio } from 'antd';
+import { Radio } from 'antd';
+import {IconFA} from "../../../components/Common/IconFA";
+import {EventFilter, EventPreview, FilterList, FilterReducerAction} from "../../../data/event/types";
 import SideCalendar from "../../../components/Calendar/SideCalendar";
 import {useTranslation} from "react-i18next";
 import { add, parse, format } from 'date-fns'
@@ -9,8 +11,43 @@ import getDay from 'date-fns/getDay'
 import {enUS, fr} from 'date-fns/locale'
 
 import 'react-big-calendar/lib/css/react-big-calendar.css';
-import {IconFA} from "../../../components/Common/IconFA";
 
+
+const initFilter = (events: EventPreview[]): EventFilter => {
+    const feeds = Array.from(new Set(events.map(e => e.target)));
+    return (
+        {
+            feeds: feeds.reduce((acc: FilterList, feed) => {
+                acc[feed] = true;
+                return acc
+            }, {}),
+            types: events.reduce((acc: FilterList, {type}) => {
+                if(!acc[type])
+                    acc[type] = true;
+                return acc
+            }, {}),
+            "publishedOnly": false
+        });
+};
+
+const reducer: React.Reducer<EventFilter, FilterReducerAction> = (filter, action) => {
+    const newFilter = {...filter};
+    switch (action.type) {
+        case "TOGGLE_FEED":
+            newFilter.feeds[action.name] = !filter.feeds[action.name];
+            break;
+        case "TOGGLE_TYPE":
+            newFilter.types[action.name] = !filter.types[action.name];
+            break;
+        case "TOGGLE_PUBLISHED":
+            newFilter.publishedOnly = !filter.publishedOnly;
+            break;
+        case "INIT_FILTER":
+            return initFilter(action.events);
+
+    }
+    return newFilter;
+};
 
 const locales = {
     'fr': fr,
@@ -26,6 +63,7 @@ const localizer = dateFnsLocalizer({
 });
 
 const Events: React.FC = () => {
+    const [filter, setFilter] = useReducer(reducer, initFilter([]));
     const [date, setDate] = useState<Date>(new Date());
     const [view, setView] = useState<View>("month");
     const {t, i18n} = useTranslation("event");
@@ -65,7 +103,7 @@ const Events: React.FC = () => {
 
     return (
         <div className="h-full flex flex-row flex-wrap bg-gray-100">
-            <SideCalendar className="md:w-1/5 w-full" date={date} handleChange={(d) => setDate(d)}/>
+            <SideCalendar className="md:w-1/5 w-full" date={date} handleDate={(d) => setDate(d)} filter={filter} handleFilter={setFilter}/>
             <div className="flex flex-col md:w-4/5 w-full h-screen p-3" style={{maxHeight: 600}}>
                 <div className="h-16 w-full flex justify-between items-center">
                     <div className="flex items-baseline">
