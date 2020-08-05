bef5d6aaa4c8721ef432b459cb6f96804c0b9b5c
Paul Vénard <pvenard@juniorisep.com>
2020-08-05 19:54:04 +0200

    style(discovery): add prupose of plateform and lorem text in translations

--- src/pages/discovery/index.tsx
+++ src/pages/discovery/index.tsx
@@ -2,13 +2,12 @@ import React from "react"
 import DiscoveryClub from "../../components/Discovery/DiscoveryClub"
 import YearBook from "../../components/Discovery/YearBook"
 import {useTranslation} from "react-i18next"
+import DiscoverPurpose from "../../components/Discovery/DiscoveryPurpose"
 
-export const MOBILE_WIDTH = 500
 
 interface SpacerProps {
     spacing?: number
 }
-
 export const HorizontalSpacer = (props: SpacerProps) => {
     const size: string = "my-" + (props.spacing || 2)
     return (
@@ -20,9 +19,14 @@ const Discovery: React.FC = () => {
     const {t} = useTranslation("discovery")
     return (
         <div>
-            <h1>{t("welcome")}</h1>
-            <div className="w-5/6 sm:w-1/2 max-w-4xl mx-auto my-8 text-sm sm:text-2xl">
+            <h1 className="text-center text-5xl font-bold text-indigo-500">{t("welcome")}</h1>
+            <div className="w-9/12 sm:w-1/2 max-w-4xl mx-auto my-8 text-sm text-center">
                 {t("welcome_description")}
+            </div>
+            <div className="my-2 flex justify-around">
+                <DiscoverPurpose img="/img/party.svg" title={t("purposes.0.title")} description={t("purposes.0.description")} />
+                <DiscoverPurpose img="/img/share.svg" title={t("purposes.1.title")} description={t("purposes.1.description")} />
+                <DiscoverPurpose img="/img/inform.svg" title={t("purposes.2.title")} description={t("purposes.2.description")} />
             </div>
             <DiscoveryClub/>
             <YearBook/>
