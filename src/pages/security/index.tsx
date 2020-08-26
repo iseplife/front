04c87317749dafd9ea79d67c48b315f5f86e95b3
Paul Vénard <pvenard@juniorisep.com>
2020-08-26 17:02:21 +0200

    fix(login): fix login page html

--- src/pages/security/index.tsx
+++ src/pages/security/index.tsx
@@ -24,6 +24,10 @@ const Login: React.FC = () => {
         onSubmit: ({id, password}) => {
             setLoadingStatus(true)
             connect(id, password)
+                .then(() => {
+                    const {from} = (location.state as { from: { pathname: string } }) || {from: {pathname: "/"}}
+                    history.replace(from)
+                })
                 .catch(e => {
                     let msg
                     if (e.response) {
@@ -41,47 +45,54 @@ const Login: React.FC = () => {
                         msg = "Serveur indisponible"
                     }
                     setError(msg)
-                }).then(() => {
-                    const {from} = (location.state as { from: { pathname: string } }) || {from: {pathname: "/"}}
-                    history.replace(from)
                 }).finally(() => setLoadingStatus(false))
         }
     })
 
     return (
-        <div className="h-full flex justify-center items-center">
-            <div className="bg-white rounded-b shadow-lg p-4 flex flex-col">
-                {isLoggedIn() && <Redirect to="/"/>}
-                {error && <div className="text-center text-red-400"><h6>{error}</h6></div>}
-                <form className="flex justify-center flex-col" onSubmit={formik.handleSubmit}>
-                    <input id="id" name="id" type="text" onChange={formik.handleChange}
-                        required
-                        placeholder={t("id")}
-                        value={formik.values.id}
-                        className="text-indigo-500 border border-indigo-200 m-3 py-2 px-5 rounded-full"
-                    />
-                    <input id="password" name="password" type="password" onChange={formik.handleChange}
-                        required
-                        placeholder={t("password")}
-                        value={formik.values.password}
-                        className="text-indigo-500 border border-indigo-200 m-3 py-2 px-5 rounded-full"
-                    />
-                    <button type="submit"
-                        disabled={loading}
-                        className={`${loading && "cursor-not-allowed"} rounded-full mt-8 py-2 px-4 bg-indigo-500 text-white hover:text-indigo-200 shadow-lg text-xl font-dinotcb`}
-                    >
-                        {loading ?
-                            <Loading/> :
-                            "let's go !"
-                        }
-                    </button>
-                </form>
+        <div className="h-full flex flex-col justify-end items-center overflow-y-auto">
+            <div className="flex-grow flex items-center">
+                <div className="bg-white rounded-b shadow-lg p-4 flex flex-col">
+                    {isLoggedIn() && <Redirect to="/"/>}
+                    {error && <div className="text-center text-red-400"><h6>{error}</h6></div>}
+                    <form className="flex justify-center flex-col" onSubmit={formik.handleSubmit}>
+                        <input
+                            id="id" name="id" type="text" onChange={formik.handleChange}
+                            required
+                            placeholder={t("id")}
+                            value={formik.values.id}
+                            className="text-indigo-500 border border-indigo-200 m-3 py-2 px-5 rounded-full"
+                        />
+                        <input
+                            id="password" name="password" type="password" onChange={formik.handleChange}
+                            required
+                            placeholder={t("password")}
+                            value={formik.values.password}
+                            className="text-indigo-500 border border-indigo-200 m-3 py-2 px-5 rounded-full"
+                        />
+                        <button
+                            type="submit"
+                            disabled={loading}
+                            className={`${loading && "cursor-not-allowed"} rounded-full mt-8 py-2 px-4 bg-indigo-500 text-white hover:text-indigo-200 shadow-lg text-xl font-dinotcb`}
+                        >
+                            {loading ?
+                                <Loading/> :
+                                "let's go !"
+                            }
+                        </button>
+                    </form>
+                </div>
             </div>
 
-            <div className="fixed bottom-0 flex flex-col items-center mb-2">
+            <div className="flex flex-col items-center mb-2">
                 <div className="flex flex-row">
                     {SUPPORTED_LANGUAGES.map(lng => (
-                        <img key={lng} className="mx-2 cursor-pointer" src={`/img/flag/${lng}.jpg`} onClick={() => i18n.changeLanguage(lng)} style={{height: 30}}
+                        <img
+                            key={lng}
+                            className="mx-2 cursor-pointer"
+                            src={`/img/flag/${lng}.jpg`}
+                            onClick={() => i18n.changeLanguage(lng)}
+                            style={{height: 30}}
                             alt={lng + " flag"}/>
                     ))}
                 </div>
