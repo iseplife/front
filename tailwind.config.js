777dc1ad179b00f1332533df5d9aa94700a9d820
Léo MELKI <leme60926@eleve.isep.fr>
2021-08-21 04:20:36 +0200

    refactor(reskin): remove useless font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,6 @@ module.exports = {
     important: true,
     theme: {
         extend: {},
-      /*  fontFamily: {
-            'body': ['PoppinsRegular'],
-            'sans': ['PoppinsRegular'],
-            'thin': ['PoppinsThin'],
-            'light': ['PoppinsLight'],
-            'med': ['PoppinsMedium'],
-            'sbold': ['PoppinsSemiBold'],
-            'bold': ['PoppinsBold'],
-        }*/
     },
     variants: {
         backgroundColor: ["responsive", "hover", "focus", "active"],
