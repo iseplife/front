3373384b5389f5dcf5e1a1b94e6aed9fe1b4bc25
Paul <dev.warthy@gmail.com>
2021-10-11 19:57:58 -0400

    fix(student): correct display of student card

--- src/components/Discovery/StudentCard.tsx
+++ src/components/Discovery/StudentCard.tsx
@@ -9,7 +9,13 @@ type StudentCardProps = {
 }
 
 const StudentCard: React.FC<StudentCardProps> = React.memo(({id, fullname, picture, promo}) => (
-    <StudentAvatar id={id} picture={picture} name={fullname} size={150} showPreview className="w-1/2 sm:w-1/2 md:w-1/3 lg:w-1/5 text-gray-700 my-2">
+    <StudentAvatar
+        id={id}
+        picture={picture}
+        name={fullname}
+        size={150}
+        showPreview className="w-1/2 sm:w-1/2 md:w-1/3 lg:w-1/5 text-gray-700 my-2 flex-col items-center"
+    >
         <div className="font-bold sm:text-xl">
             <p className="-mb-2">{fullname}</p>
             <span className="italic text-xs sm:text-sm">
