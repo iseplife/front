32a5cf2e5f4b2eede236d2a821ced1411265c4c9
Léo MELKI <leme60926@eleve.isep.fr>
2021-12-22 17:13:19 +0100

    feat(notifications-center): watch notifications when openning menu + ...
    + scrollbar
    + limitation de la hauteur max du menu pour que ça ne dépasse pas la hauteur de l'écran (merci tailwindcss v3 !)

--- src/components/Notification/NotificationsCenter.tsx
+++ src/components/Notification/NotificationsCenter.tsx
@@ -1,17 +1,29 @@
-import React, { useContext, useEffect, useState } from "react"
+import React, { useContext, useEffect, useMemo, useState } from "react"
 import Notification from "."
 import { AppContext } from "../../context/app/context"
-import { loadNotifications } from "../../data/notification"
+import { loadNotifications, setNotificationsWatched } from "../../data/notification"
 import { Notification as NotificationObject } from "../../data/notification/types"
+import { AppActionType } from "../../context/app/action"
 
 const NotificationsCenter: React.FC = () => {
-    const {state: {user: {unwatchedNotifications}}} = useContext(AppContext)
+    const { state: { user }, dispatch } = useContext(AppContext)
+    
+    const unwatchedNotifications = useMemo(() => user.unwatchedNotifications, [])
+
     const [notifications, setNotifications] = useState([] as NotificationObject[])
     useEffect(() => {
-        loadNotifications(0).then(notifs => setNotifications(notifs.data.content))
+        loadNotifications(0).then(notifs => {
+            setNotifications(notifs.data.content)
+            const unwatched = notifs.data.content.filter(notif => !notif.watched)
+            setNotificationsWatched(...unwatched)
+            dispatch({
+                type: AppActionType.SET_LOGGED_USER,
+                user: { ...user, unwatchedNotifications: user.unwatchedNotifications - unwatched.length }
+            })
+        })
     }, [])
     return (
-        <div className="fixed top-16 right-6 rounded-lg shadow-lg w-80 max-h-96 bg-white pb-2">
+        <div className="fixed top-16 right-6 rounded-lg shadow-lg w-80 max-h-[calc(100vh-4rem-1rem)] bg-white pb-2 overflow-auto scrollbar-thin">
             <div className="font-bold text-2xl px-4 py-2.5">Notifications{!!unwatchedNotifications && ` (${unwatchedNotifications})`}</div>
             {notifications.map(notif =>
                 <Notification {...notif} key={notif.id}></Notification>
--- src/data/notification/index.ts
+++ src/data/notification/index.ts
@@ -4,3 +4,4 @@ import { Page } from "../request.type"
 import { Notification } from "./types"
 
 export const loadNotifications = (page: number) : AxiosPromise<Page<Notification>> => apiClient.get(`/notifications/${page}`)
+export const setNotificationsWatched = (...notifications: Notification[]) => notifications.length && apiClient.post("/notifications/watch", notifications.reduce((form, val) => {form.append("ids[]", val.id.toString()); return form}, new FormData()))
