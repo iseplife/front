f0911af5e061c2eb25da6f27c02a5b3e10691599
Léo MELKI <leme60926@eleve.isep.fr>
2021-12-22 17:31:32 +0100

    feat(notifications-center): added long ago section (+1w)

--- src/components/Notification/NotificationsCenter.tsx
+++ src/components/Notification/NotificationsCenter.tsx
@@ -4,16 +4,24 @@ import { AppContext } from "../../context/app/context"
 import { loadNotifications, setNotificationsWatched } from "../../data/notification"
 import { Notification as NotificationObject } from "../../data/notification/types"
 import { AppActionType } from "../../context/app/action"
+import { isAfter, isBefore } from "date-fns"
+import moment from "moment"
+import { Divider } from "antd"
 
 const NotificationsCenter: React.FC = () => {
     const { state: { user }, dispatch } = useContext(AppContext)
     
     const unwatchedNotifications = useMemo(() => user.unwatchedNotifications, [])
 
-    const [notifications, setNotifications] = useState([] as NotificationObject[])
+    const [newNotifications, setNewNotifications] = useState([] as NotificationObject[])
+    const [oldNotifications, setOldNotifications] = useState([] as NotificationObject[])
     useEffect(() => {
         loadNotifications(0).then(notifs => {
-            setNotifications(notifs.data.content)
+            const oneWeekAgo = moment().add(-1, "week").toDate()
+
+            setNewNotifications(notifs.data.content.filter(notif => isAfter(notif.creation, oneWeekAgo)))
+            setOldNotifications(notifs.data.content.filter(notif => isBefore(notif.creation, oneWeekAgo)))
+
             const unwatched = notifs.data.content.filter(notif => !notif.watched)
             setNotificationsWatched(...unwatched)
             dispatch({
@@ -25,9 +33,14 @@ const NotificationsCenter: React.FC = () => {
     return (
         <div className="fixed top-16 right-6 rounded-lg shadow-lg w-80 max-h-[calc(100vh-4rem-1rem)] bg-white pb-2 overflow-auto scrollbar-thin">
             <div className="font-bold text-2xl px-4 py-2.5">Notifications{!!unwatchedNotifications && ` (${unwatchedNotifications})`}</div>
-            {notifications.map(notif =>
+            {newNotifications.map(notif =>
                 <Notification {...notif} key={notif.id}></Notification>
             )}
+            {!!oldNotifications?.length && <Divider className="text-gray-700 text-base" orientation="left">Il y a longtemps</Divider>}
+            {oldNotifications.map(notif =>
+                <Notification {...notif} key={notif.id}></Notification>
+            )}
+
         </div>
     )
 }
