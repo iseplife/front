f3a75d54d5f571b52e57d8e6132307954b56b90f
Léo MELKI <melkileo@gmail.com>
2022-02-05 02:00:24 +0100

    feat(reskin-group): lighter three dots background on compressed members

--- src/components/Common/CompressedMembers.tsx
+++ src/components/Common/CompressedMembers.tsx
@@ -14,7 +14,7 @@ const CompressedMembers: React.FC<CompressedMembersProps> = ({ members, classNam
     return <div className={"flex " + className} onClick={onClick}> {
         members.slice(0, 9).map((student, index, array) =>
             <div className="-mr-1.5 relative" style={{ zIndex: 100 - index }} title={`${student.firstName} ${student.lastName}`} key={index}>
-                {array.length - 1 == index && array.length != members.length && <div className="rounded-full bg-black bg-opacity-50 absolute w-full h-full z-10 text-gray-300 items-center justify-center flex text-xs border-gray-100 border-2">
+                {array.length - 1 == index && array.length != members.length && <div className="rounded-full bg-black/[42%] absolute w-full h-full z-10 text-gray-300 items-center justify-center flex text-xs border-gray-100 border-2">
                     <div className="transform scale-50 flex">
                         <FontAwesomeIcon icon={faCircle} size="xs" className="mr-0.5"></FontAwesomeIcon>
                         <FontAwesomeIcon icon={faCircle} size="xs" className="mr-0.5"></FontAwesomeIcon>
