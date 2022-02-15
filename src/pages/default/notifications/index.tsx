6e106bd7a8d26cc042ac65d2b3ea1678ba01feb1
Léo MELKI <leme60926@eleve.isep.fr>
2022-02-15 17:17:50 +0100

    refactor(notifications-page): removed useless code

--- src/pages/default/notifications/index.tsx
+++ src/pages/default/notifications/index.tsx
@@ -1,4 +1,4 @@
-import React, {useCallback, useContext, useMemo, useState} from "react"
+import React, {useCallback, useContext, useEffect, useMemo, useState} from "react"
 import {useTranslation} from "react-i18next"
 import {AppContext} from "../../../context/app/context"
 import {Notification as NotificationType} from "../../../data/notification/types"
@@ -18,59 +18,6 @@ const NotificationsPage: React.FC = () => {
     const unwatchedNotifications = useMemo(() => user.unwatchedNotifications, [])
 
     const [notificationIds, setNotificationIds] = useState<Set<number>>(new Set())
-    const [recentNotifications, setRecentNotifications] = useState<NotificationType[]>([])
-    const [oldNotifications, setOldNotifications] = useState<NotificationType[]>([])
-
-    const [loading, setLoading] = useState<boolean>(true)
-    const [empty, setEmpty] = useState<boolean>(false)
-
-    const loadedNotifications = useMemo(() => (
-        oldNotifications.length + recentNotifications.length
-    ), [oldNotifications, recentNotifications])
-
-    const main = useMemo(() => document.getElementById("main"), [])
-
-    const loadMoreNotifications = useCallback(async (count: number) => {
-        setLoading(true)
-        const res = await loadNotifications(count)
-
-        const oneWeekAgo = add(new Date(), {weeks: -1})
-        const old: NotificationType[] = []
-        const recent: NotificationType[] = []
-        const ids: number[] = []
-        res.data.content.forEach(notif => {
-            if(!notificationIds.has(notif.id)){
-                const notifGroup = isAfter(notif.creation, oneWeekAgo) ? recent : old
-                notifGroup.push(notif)
-                ids.push(notif.id)
-            }
-        })
-
-        setRecentNotifications(notifs => [...notifs, ...recent])
-        setOldNotifications(notifs => [...notifs, ...old])
-        setNotificationIds(set => {
-            ids.forEach(id => set.add(id))
-            return set
-        })
-
-        setLoading(false)
-
-        const unwatched = res.data.content.filter(notif => !notif.watched)
-        if (unwatched.length) {
-            const unwatchedCount = (await setNotificationsWatched(unwatched)).data
-            if (unwatchedCount)
-                dispatch({
-                    type: AppActionType.SET_UNWATCHED_NOTIFICATIONS,
-                    payload: unwatchedCount
-                })
-        }
-
-        if (count === 0 && res.data.content.length === 0)
-            setEmpty(true)
-
-        return res.data.last
-    }, [notificationIds])
-
 
     return (
         <div className="sm:mt-5 flex container mx-auto">
@@ -80,8 +27,8 @@ const NotificationsPage: React.FC = () => {
             <div className="mx-4 md:mx-10 sm:col-span-2 flex flex-grow flex-col p-4 shadow-sm rounded-lg bg-white my-5">
                     
                 <div className="flex-1">
-                    <div className="mb-5 items-center cursor-pointer flex">
-                        <h3 className="text-black mx-2 mb-0 font-bold text-2xl text-gray-700 leading-4 mt-1 block">
+                    <div className="mb-5 items-center flex">
+                        <h3 className="text-black mx-2 mb-0 font-bold text-2xl leading-4 mt-1 block">
                             {t("notifications")}{!!unwatchedNotifications && ` (${unwatchedNotifications})`}
                         </h3>
                     </div>
