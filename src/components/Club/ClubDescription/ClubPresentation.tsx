10d5d593d9eaee860ddcd593607bfab2e93352a4
leomelki <melkileo@gmail.com>
2022-03-01 15:54:04 +0100

    feat(galleries-preview): added container to component

--- src/components/Club/ClubDescription/ClubPresentation.tsx
+++ src/components/Club/ClubDescription/ClubPresentation.tsx
@@ -15,25 +15,13 @@ const ClubPresentation: React.FC = () => {
     const {t} = useTranslation(["club", "gallery"])
     const { club: { id } } = useContext(ClubContext)
 
-    const [galleriesVisible, setGalleriesVisible] = useState<boolean>(false)
-
-    const setVisible = useCallback(() => setGalleriesVisible(true), [])
-
     return (
         <div key="desktop-display" className="w-full">
             <div className="flex flex-col px-4 py-3 shadow-sm rounded-lg bg-white mb-5 mt-5 sm:mt-0">
                 <span className="text-neutral-900 font-semibold text-base">{t("about")}</span>
                 <About/>
             </div>
-            <div className="flex-col px-4 py-3 shadow-sm rounded-lg bg-white my-5 hidden sm:flex">
-                <div className="text-neutral-900 font-semibold text-base flex items-center -mt-1">
-                    <span>{t("club:galleries")}</span>
-                    <div onClick={setVisible}  className="ml-auto py-1 -mr-1 hover:bg-black/5 transition-colors px-2 rounded text-indigo-500 font-normal text-sm grid place-items-center cursor-pointer mt-1">
-                        {t("gallery:see_all")}
-                    </div>
-                </div>
-                <GalleriesPreview visible={galleriesVisible} setVisible={setGalleriesVisible} elementId={id} getGalleriesCallback={getClubGalleries} />
-            </div>
+            <GalleriesPreview elementId={id} getGalleriesCallback={getClubGalleries} />
         </div>
 
     )
--- src/components/Gallery/GalleriesPreview.tsx
+++ src/components/Gallery/GalleriesPreview.tsx
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect, useLayoutEffect, useState} from "react"
+import React, {useCallback, useContext, useEffect, useLayoutEffect, useState} from "react"
 import {GalleryPreview} from "../../data/gallery/types"
 import GalleryCard from "./GalleryCard"
 import {useTranslation} from "react-i18next"
@@ -13,16 +13,19 @@ import { AxiosPromise } from "axios"
 import { Page } from "../../data/request.type"
 
 interface GalleriesPreviewProps {
-    visible: boolean,
-    setVisible: (visible: boolean) => void
     elementId: number
     getGalleriesCallback: (id: number, page?: number) => AxiosPromise<Page<GalleryPreview>>
+    className?: string
 }
 
-const GalleriesPreview: React.FC<GalleriesPreviewProps> = ({visible, setVisible, elementId, getGalleriesCallback}) => {
+const GalleriesPreview: React.FC<GalleriesPreviewProps> = ({elementId, getGalleriesCallback, className = ""}) => {
     const {t} = useTranslation("gallery")
     const [loading, setLoading] = useState<boolean>()
     const [galleriesPreview, setGalleriesPreview] = useState<GalleryPreview[]>([])
+
+    const [galleriesVisible, setGalleriesVisible] = useState<boolean>(false)
+    const openModal = useCallback(() => setGalleriesVisible(true), [])
+    const closeModal = useCallback(() => setGalleriesVisible(false), [])
 
     useEffect(() => {
         setLoading(true)
@@ -35,27 +38,36 @@ const GalleriesPreview: React.FC<GalleriesPreviewProps> = ({visible, setVisible,
 
     }, [elementId])
 
-    return loading ?
-        <GalleriesPreviewSkeleton/> :
-        <div className="-mt-1">
-            {galleriesPreview.map(g => (
-                <GalleryCard key={g.id} gallery={g}/>
-            ))}
-            {galleriesPreview.length == 0 && 
-                <div className="text-center text-gray-400">
-                    <FontAwesomeIcon icon={faCameraRetro} size="4x"/>
-                    <p>{t("no_gallery")}</p>
-                </div>
-            }
-            <Modal
-                title={<h1 className="text-gray-800 font-bold text-xl m-0">{t("galleries")}</h1>}
-                visible={visible}
-                onCancel={() => setVisible(false)}
-                footer={null}
-            >
-                <GalleriesModal elementId={elementId} getGalleriesCallback={getGalleriesCallback}/>
-            </Modal>
+    return <div className={`flex-col px-4 py-3 shadow-sm rounded-lg bg-white my-5 hidden sm:flex ${className}`}>
+        <div className="text-neutral-900 font-semibold text-base flex items-center -mt-1">
+            <span>{t("club:galleries")}</span>
+            <div onClick={openModal}  className="ml-auto py-1 -mr-1 hover:bg-black/5 transition-colors px-2 rounded text-indigo-500 font-normal text-sm grid place-items-center cursor-pointer mt-1">
+                {t("gallery:see_all")}
+            </div>
         </div>
+        
+        {loading ? <GalleriesPreviewSkeleton /> :
+            <div className="-mt-1">
+                {galleriesPreview.map(g => (
+                    <GalleryCard key={g.id} gallery={g} />
+                ))}
+                {galleriesPreview.length == 0 &&
+                    <div className="text-center text-gray-400">
+                        <FontAwesomeIcon icon={faCameraRetro} size="4x" />
+                        <p>{t("no_gallery")}</p>
+                    </div>
+                }
+                <Modal
+                    title={<h1 className="text-gray-800 font-bold text-xl m-0">{t("galleries")}</h1>}
+                    visible={galleriesVisible}
+                    onCancel={closeModal}
+                    footer={null}
+                >
+                    <GalleriesModal elementId={elementId} getGalleriesCallback={getGalleriesCallback} />
+                </Modal>
+            </div>
+        }
+    </div>
 }
 
 export default GalleriesPreview
