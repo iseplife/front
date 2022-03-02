6b317df248307d41a1569e5a8225027b3c1c2a1b
leomelki <melkileo@gmail.com>
2022-03-02 17:45:55 +0100

    feat(reskin-event-sidebar): add event type to calendar

--- src/components/Event/EventPreview.tsx
+++ src/components/Event/EventPreview.tsx
@@ -9,7 +9,7 @@ type EventProps = {
     event: PreviewType
 }
 const EventPreview: React.FC<EventProps> = ({ event }) => {
-    const { i18n: { language } } = useTranslation()
+    const {t,  i18n: { language } } = useTranslation("event")
     return (
         <Link to={`/event/${event.id}`} className="w-full max-w-sm text-gray-700 hover:text-gray-500">
             <div
@@ -17,13 +17,16 @@ const EventPreview: React.FC<EventProps> = ({ event }) => {
                 className="flex flex-col sm:flex-row px-3 py-2 shadow-sm rounded-lg bg-white items-center"
             >
                 <div className="text-center pb-1">
-                    <div className="mx-auto sm:mx-0 w-12 h-12 text-2xl md:w-14 md:h-14 md:text-3xl rounded-md bg-neutral-100 shadow-sm overflow-hidden font-medium relative flex flex-col flex-shrink-0">
-                        <div className="bg-red-500 w-full h-4 text-xs leading-4 md:leading-5 md:text-sm md:h-5 flex-shrink-0  uppercase">
-                            {format(event.startsAt, "MMM", {locale: getLocaleFromTranslation(language)})}
+                    <div className="w-12 h-12 md:w-14 md:h-14 relative">
+                        <div className="w-full h-full mx-auto sm:mx-0 text-2xl md:text-3xl rounded-md bg-neutral-100 shadow-sm overflow-hidden font-medium relative flex flex-col flex-shrink-0">
+                            <div className="bg-red-500 w-full h-4 text-xs leading-4 md:leading-5 md:text-sm md:h-5 flex-shrink-0  uppercase">
+                                {format(event.startsAt, "MMM", {locale: getLocaleFromTranslation(language)})}
+                            </div>
+                            <div className="grid place-items-center h-full">
+                                { event.startsAt.getDate() }
+                            </div>
                         </div>
-                        <div className="grid place-items-center h-full">
-                            { event.startsAt.getDate() }
-                        </div>
+                        <div className="absolute -top-2.5 -right-2.5 text-lg rotate-12" title={t(`type.${event.type}`)}>{t(`type.${event.type}`).replace(/[A-z]| /g, "")}</div>
                     </div>
                 </div>
                 <span className="flex-1 text-left font-semibold text-lg -mb-1 sm:mb-0 sm:text-xl truncate sm:ml-2.5 xl:ml-3.5">
