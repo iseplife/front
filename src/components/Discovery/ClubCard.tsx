492fdc1bf5a339122eed304246c716916e1be490
leomelki <melkileo@gmail.com>
2022-03-02 18:44:25 +0100

    fix(reskin-discovery): removed useless div

--- src/components/Discovery/ClubCard.tsx
+++ src/components/Discovery/ClubCard.tsx
@@ -9,23 +9,21 @@ type ClubCardProps = {
 }
 
 const ClubCard: React.FC<ClubCardProps> = ({club}) => (
-    <div className="my-2 w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/5 xl:w-1/6">
-        <Link to={`/club/${club.id}`}>
-            <div
-                className="mx-1 bg-white rounded-2xl overflow-hidden hover:shadow-sm transition-shadow h-52 px-4 items-end flex aspect-[18/20]"
-                style={{
-                    backgroundImage: `url("${mediaPath(club.logoUrl, AvatarSizes.FULL)}")`,
-                    backgroundRepeat: "no-repeat",
-                    backgroundSize: "147%",
-                    backgroundPosition: "center",
-                }}
-            >
-                <div className="bg-white/40 rounded-xl backdrop-blur grid place-items-center px-3 py-2 mb-3 mt-auto w-full text-neutral-800 font-semibold text-2xl">
-                    {club.name}
-                </div>
+    <Link to={`/club/${club.id}`}>
+        <div
+            className="mx-1 bg-white rounded-2xl overflow-hidden hover:shadow-sm transition-shadow h-52 px-4 items-end flex aspect-[18/20]"
+            style={{
+                backgroundImage: `url("${mediaPath(club.logoUrl, AvatarSizes.FULL)}")`,
+                backgroundRepeat: "no-repeat",
+                backgroundSize: "147%",
+                backgroundPosition: "center",
+            }}
+        >
+            <div className="bg-white/40 rounded-xl backdrop-blur grid place-items-center px-3 py-2 mb-3 mt-auto w-full text-neutral-800 font-semibold text-2xl">
+                {club.name}
             </div>
-        </Link>
-    </div>
+        </div>
+    </Link>
 )
 
 export default ClubCard
