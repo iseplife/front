472aab06e590df0707cedbef44f82d05a2c2a063
Léo MELKI <melkileo@gmail.com>
2022-03-02 11:23:56 +0100

    feat(reskin-discover): added floating icons & first title

--- src/pages/default/discovery/index.tsx
+++ src/pages/default/discovery/index.tsx
@@ -6,28 +6,42 @@ import { AppContext } from "../../../context/app/context"
 const Discovery: React.FC = () => {
     const { t } = useTranslation("discovery")
     const { state: { user } } = useContext(AppContext)
-    return (
-        <div className="container mx-auto px-4 font-bold py-40 block md:flex">
-            <div>
-                <div className="flex text-4xl items-center">
-                    <div className="text-neutral-600">Bonjour</div>
-                    <img src="/img/icons/hello.svg" className="ml-3 w-11 h-11 mt-1" />
+    return (<>
+        <div className="z-0 pointer-events-none opacity-60">
+            <img src="img/icons/floating/square.svg" className="fixed top-[450px] md:top-[400px] left-1/4" />
+            <img src="img/icons/floating/camera.svg" className="fixed top-[450px] right-[10%] md:block hidden" />
+            <img src="img/icons/floating/triangle.svg" className="fixed top-[550px] right-[40%] translate-x-32" />
+            <img src="img/icons/floating/star.svg" className="fixed top-[700px] left-[5%]" />
+        </div>
+
+        <div className="container mx-auto px-4 font-bold text-neutral-700">
+            <div className="py-40 block md:flex">
+                <div>
+                    <div className="flex text-4xl items-center">
+                        Bonjour
+                        <img src="/img/icons/hello.svg" className="ml-3 w-11 h-11 mt-1" />
+                    </div>
+                    <div className="text-indigo-400 text-6xl">{ user.firstName }</div>
                 </div>
-                <div className="text-indigo-400 text-6xl">{ user.firstName }</div>
+                <div className="flex text-lg ml-auto mr-0 items-center mt-6 md:mt-0 text-center">
+                    <Link to="/">
+                        <div className="bg-indigo-400 text-white rounded-xl w-48 py-[11.5px] grid place-items-center mr-5">
+                            Mon Feed
+                        </div>
+                    </Link>
+                    <Link to={{pathname: "https://github.com/iseplife"}} target="_blank">
+                        <div className="border-indigo-400 text-indigo-400 border-[3.5px] rounded-xl w-48 py-2 grid place-items-center">
+                            Voir sur GitHub
+                        </div>
+                    </Link>
+                </div>
+
             </div>
-            <div className="flex text-lg ml-auto mr-0 items-center mt-6 md:mt-0 text-center">
-                <Link to="/">
-                    <div className="bg-indigo-400 text-white rounded-xl w-48 py-[11.5px] grid place-items-center mr-5">
-                        Mon Feed
-                    </div>
-                </Link>
-                <Link to={{pathname: "https://github.com/iseplife"}} target="_blank">
-                    <div className="border-indigo-400 text-indigo-400 border-[3.5px] rounded-xl w-48 py-2 grid place-items-center">
-                        Voir sur GitHub
-                    </div>
-                </Link>
+            <div>
+                <div className="bg-indigo-400 rounded-full w-16 h-3"></div>
+                <div className="text-2xl mt-2">La vie étudiante simplifiée</div>
             </div>
         </div>
-    )
+    </>)
 }
 export default Discovery
