64c2db009e608502ca8b40b7789577cf5030b28b
leomelki <melkileo@gmail.com>
2022-03-02 14:57:48 +0100

    feat(reskin-discovery): fix name line height

--- src/components/Discovery/StudentCard.tsx
+++ src/components/Discovery/StudentCard.tsx
@@ -20,7 +20,7 @@ const StudentCard: React.FC<StudentCardProps> = React.memo(({id, fullname, pictu
         <div className="absolute right-2.5 top-2.5 bg-white/30 rounded-lg backdrop-blur grid place-items-center px-2 py-0.5 text-neutral-600 font-normal text-lg">
             {promo}
         </div>
-        <div className="bg-white/40 rounded-xl backdrop-blur grid place-items-center px-3 py-2 mb-3 mt-auto w-full text-neutral-800 font-semibold text-xl">
+        <div className="bg-white/40 rounded-xl backdrop-blur grid place-items-center px-3 py-2 leading-6 mb-3 mt-auto w-full text-neutral-800 font-semibold text-xl">
             {fullname}
         </div>
     </div>
