5b2583e69625b829c9f05eaa72674f616989a1e1
Léo MELKI <melkileo@gmail.com>
2022-03-04 12:06:14 +0100

    refactor(notifications): moved sub extensive button to own component

--- src/components/Club/ClubHeader.tsx
+++ src/components/Club/ClubHeader.tsx
@@ -12,6 +12,7 @@ import { SubscribableType } from "../../data/subscription/SubscribableType"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faBell as faSolidBell, faCheck } from "@fortawesome/free-solid-svg-icons"
 import { faBell } from "@fortawesome/free-regular-svg-icons"
+import SubscriptionExtensiveButton from "../Subscription/SubscriptionExtensiveButton"
 
 const ClubHeader: React.FC = () => {
     const {club, dispatch} = useContext(ClubContext)
@@ -36,19 +37,14 @@ const ClubHeader: React.FC = () => {
             })
         }
     }, [club])
-    const handleExtensive = useCallback(() => {
-        if (club) {
-            const newExtensive = !club.subscribed?.extensive
-            subscribe(club.id, SubscribableType.CLUB, newExtensive).then(_ => {
-                dispatch({
-                    type: ClubActionType.UPDATE_CLUB,
-                    payload: {
-                        ...club,
-                        subscribed: { extensive: newExtensive },
-                    }
-                })
-            })
-        }
+    const handleExtensive = useCallback((newExtensive: boolean) => {
+        dispatch({
+            type: ClubActionType.UPDATE_CLUB,
+            payload: {
+                ...club,
+                subscribed: { extensive: newExtensive },
+            }
+        })
     }, [club])
 
     return (
@@ -66,12 +62,7 @@ const ClubHeader: React.FC = () => {
                         <div className="flex">
                             <div onClick={handleSubscription} className={"py-1.5 cursor-pointer select-none rounded-full px-3.5 text-white text-base items-center flex " + (club.subscribed ? "border-blue-500 border-2 text-blue-500 hover:text-opacity-80 hover:border-opacity-80" : "bg-blue-500 hover:bg-opacity-90")}>{club.subscribed ? "Unfollow" : "Follow"}</div>
                             {club.subscribed &&
-                                <div className="w-12 grid place-items-center relative ml-0 mr-auto">
-                                    <FontAwesomeIcon onClick={handleExtensive} icon={club.subscribed.extensive ? faSolidBell : faBell} className="text-blue-500 text-2xl cursor-pointer hover:text-opacity-90" />
-                                    {club.subscribed?.extensive &&
-                                        <FontAwesomeIcon icon={faCheck} className="top-1.5 right-[3px] pointer-events-none text-blue-500 cursor-pointer text-sm absolute transform scale-75" />
-                                    }
-                                </div>
+                                <SubscriptionExtensiveButton updateExtensive={handleExtensive} extensive={club?.subscribed?.extensive} id={club?.id} type={SubscribableType.CLUB} />
                             }
                         </div>
                     </div>
