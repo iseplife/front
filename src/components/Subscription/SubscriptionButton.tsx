e79bb369094189be0ba96954b83ec2578f8ade01
leomelki <melkileo@gmail.com>
2022-03-07 19:01:23 +0100

    feat(notifications): added translation

--- src/components/Subscription/SubscriptionButton.tsx
+++ src/components/Subscription/SubscriptionButton.tsx
@@ -1,4 +1,5 @@
-import React, { useCallback } from "react"
+import React, { MouseEvent, useCallback, useState } from "react"
+import { useTranslation } from "react-i18next"
 import { subscribe, unsubscribe } from "../../data/subscription"
 import { SubscribableType } from "../../data/subscription/SubscribableType"
 
@@ -10,17 +11,35 @@ interface SubscriptionButtonProps {
 }
 
 const SubscriptionButton: React.FC<SubscriptionButtonProps> = ({ id, type, subscribed, updateSubscription }) => {
+    const { t } = useTranslation("common")
+    const [minWidth, setMinWidth] = useState(0)
+
     const handleSubscription = useCallback(() => {
         if (id != undefined) {
             const wasSubscribed = subscribed;
             (subscribed ? unsubscribe : subscribe)(id, type).then(_ => updateSubscription(!wasSubscribed))
+
+            setMinWidth(0)
         }
     }, [id, type, subscribed, updateSubscription])
+
+    const mouseEnterCallback = useCallback((event: MouseEvent) => {
+        if(subscribed)
+            setMinWidth((event.target as HTMLDivElement).clientWidth + 4)
+    }, [subscribed])
+
     return <div 
         onClick={handleSubscription}
-        className={"h-10 font-bold cursor-pointer select-none rounded-full px-3.5 text-white text-base items-center flex " + (subscribed ? "border-blue-400 border-2 text-blue-400 hover:text-opacity-80 hover:border-opacity-80" : "bg-blue-400 hover:bg-opacity-90")}
+        className={
+            "h-10 font-bold cursor-pointer select-none rounded-full text-white text-base grid place-items-center group "
+            + (subscribed ? "border-blue-400 border-2 text-blue-400 hover:text-red-500 hover:border-red-500 hover:bg-red-400/10 px-3.5" : "bg-blue-400 hover:bg-opacity-90 px-5")
+        }
+        style={{ minWidth }}
+        onMouseEnter={mouseEnterCallback}
+        onMouseMove={mouseEnterCallback}
     >
-        {subscribed ? "Unfollow" : "Follow"}
+        {subscribed && <div className="hidden group-hover:block text-center">{t("subscription.unsubscribe")}</div>}
+        <div className={subscribed ? "group-hover:hidden text-center" : ""}>{subscribed ? t("subscription.subscribed") : t("subscription.subscribe")}</div>
     </div>
 }
 
