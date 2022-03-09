f5a7053c0b231d8c72a05b553af4a70a9a80bc59
Léo MELKI <melkileo@gmail.com>
2022-03-09 17:15:12 +0100

    feat(realtime): fix notifications overlay for small devices

--- src/components/Notification/NotificationOverlay.tsx
+++ src/components/Notification/NotificationOverlay.tsx
@@ -28,7 +28,7 @@ const NotificationsOverlay: React.FC = () => {
         }
     }, [])
 
-    return <div className="fixed bottom-3 left-3 flex flex-col gap-3 w-80">
+    return <div className="fixed left-3 flex-col gap-3 w-80 hidden sm:flex bottom-16 md:bottom-3">
         { notifications?.map(notif => 
             <div onClick={clickNotifFactory(notif)}>
                 <Notification {...notif} key={notif.id} backgroundHover={false} className={"bg-white shadow-sm hover:bg-[#fcfcfc] hover:shadow-md transition-all"} />
