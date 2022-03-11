bd495ced765494a0cd85a1d7f6576a64ca1ebfc0
Léo MELKI <melkileo@gmail.com>
2022-03-11 16:24:13 +0100

    feat(realtime): WIP feed impl

--- src/datamanager/FeedsManager.ts
+++ src/datamanager/FeedsManager.ts
@@ -1,21 +1,23 @@
-import { Notification } from "../data/notification/types"
 import DataManager from "./DataManager"
 import { getWebSocket, WSServerClient } from "../realtime/websocket/WSServerClient"
 import PacketHandler from "../realtime/protocol/listener/PacketHandler"
 import WSPSFeedPostCreated from "../realtime/protocol/v1/packets/server/WSPSFeedPostCreated"
+import { getFeedPost } from "../data/feed"
+import { Post } from "../data/post/types"
 
-export default class FeedsManager extends DataManager<Notification> {
+export default class FeedsManager extends DataManager<ManagerPost> {
+
+    private static PAGE_SIZE = 10
+
+    lastLoadId = Math.round(Math.random() * 10_000_000)
 
     constructor(wsServerClient: WSServerClient) {
-        super("notifications", ["id", "feedId", "pinned", "lastLoadId"], wsServerClient)
+        super("feeds", ["id", "feedId", "pinned", "lastLoadId", "loadedFeed"], wsServerClient)
     }
 
     protected async initData() {
-        this.initFeedData()
-    }
-
-    public async initFeedData(feed?: number){
-        await this.loadPage(undefined, 0)
+        this.maxIdByPage = {}
+        this.lastLoadId = Math.round(Math.random() * 10_000_000)
     }
 
     private async _waitFetching() {
@@ -23,39 +25,116 @@ export default class FeedsManager extends DataManager<Notification> {
             await new Promise<void>(executor => this._waitingForFetchEnd.push(executor))
     }
 
-    protected async loadPage(feed: number | undefined, page: number) {
+    private initFeedData(feed: FeedId){
+        this.loadPage(feed, 0)
+    }
+
+    private maxIdByPage: {[key: number]: number} = {}
+
+    public getFeedNotifications(feed: FeedId, page: number, limit: number){
+        return this.getTable().where({ "loadedFeed": feed ?? mainFeedId }).limit(limit)
+    }
+
+    public async loadPage(feed: FeedId, page: number) {
         await this._waitFetching()
-            
-        if (await this.isLastPage(feed))
-            return true
         
-        return await this.isLastPage(feed)
+        const posts = (await getFeedPost(feed, page)).data
+
+        FeedsManager.PAGE_SIZE = posts.size
+
+        // Update lastLoadId
+        const content = posts.content.map(post => ({
+            ...post,
+             lastLoadId: this.lastLoadId,
+             loadedFeed: feed ?? mainFeedId,
+        } as ManagerPost));
+
+        this.addBulkData(content)
+
+        const maxId = posts.content.reduce((prev, post) => Math.max(prev, post.id), 0)
+        const lastId = posts.content.reduce((prev, post) => Math.min(prev, post.id), Number.MAX_SAFE_INTEGER)
+        this.maxIdByPage[page] = maxId
+
+        if(posts.last)
+            this.setFeedLastId(feed, lastId)
+
+        // Remove deleted
+        if(this.maxIdByPage[page - 2])
+            this.checkUnloaded(feed, lastId, this.maxIdByPage[page - 2])
+        else if(this.maxIdByPage[page - 1])
+            this.checkUnloaded(feed, lastId, this.maxIdByPage[page - 1])
+        else
+            this.checkUnloaded(feed, lastId, maxId)
+
+        return posts.last
     }
 
-    async isLastPage() {
-        return await this.getMinFresh(feed) == -1 && await this.didLastPage()
+    private async getFeedPosts(feed: FeedId){
+        return this.getTable().where("loadedFeed").equals(feed ?? mainFeedId).toArray()
     }
 
-    public async watch(notifications: Notification[]) {
-        notifications.forEach(notif => notif.watched = true)
-        this.getTable().bulkPut(notifications)
+    private async setFeedLastId(feed: FeedId, lastId: number){
+        await this.setContext(`last:${feed}`, { lastId })
     }
+    private async getFeedLastId(feed: FeedId){
+        return (await this.getContext(`last:${feed}`)).lastId as number | undefined
+    }
+
 
     public async loadMore(feed: number | undefined, minId: number) {
         await this._waitFetching()
         // return await this.loadPage(feed, Math.floor(await this.getTable().where("id").aboveOrEqual(minId).count() / await this.getResultsByPage()))
     }
 
-    public async getNotifications(minId: number): Promise<Notification[]> {
-        return this.getTable().where("id").aboveOrEqual(minId).reverse().sortBy("id")
-    }
     protected async isAnyInCache(ids: number[]): Promise<boolean> {
-        return (await this.getTable().where("id").anyOf(ids).count()) > 0
+        return !!(await this.getTable().where("id").anyOf(ids).first())
+    }
+    private async isAnyOfFeedInCache(feed: FeedId, ids: number[]){
+        return !!(await this.getTable().where("loadedFeed").equals(feed ?? mainFeedId).and(post => ids.includes(post.id)).first())
+    }
+    private async checkUnloaded(feed: FeedId, minId: number, maxId: number){
+        const posts = (await this.getTable().where(["loadedFeed", "id"]).between([feed ?? mainFeedId, minId], [feed ?? mainFeedId, maxId], true, true).toArray()).sort((a, b) => a.id - b.id)
+        
+        const toUnload: ManagerPost[] = []
+        let toUnloadTemp: ManagerPost[] = []
+
+        let freshBefore = false;
+        //De l'id - au +
+        for(const post of posts){
+            const fresh = this.isFresh(post)
+            if(fresh){
+                if(freshBefore){
+                    toUnload.push(...toUnloadTemp)
+                    toUnloadTemp = []
+                }
+                freshBefore = true
+            } else
+                toUnloadTemp.push(post)
+        }
+
+        console.log("Unload posts:", toUnload)
+        await this.getTable().bulkDelete(toUnload.map(post => post.id))
+    }
+
+    public isFresh(post: ManagerPost){
+        return post.lastLoadId == this.lastLoadId
     }
 
     @PacketHandler(WSPSFeedPostCreated)
     private async handleFeedPostCreated(packet: WSPSFeedPostCreated){
-        this.addData(packet.post)//TODO work
+        this.addData({
+            ...packet.post,
+            lastLoadId: this.lastLoadId,
+            loadedFeed: packet.post.feedId
+        } as ManagerPost)
+
+        if(packet.follow){
+            this.addData({
+                ...packet.post,
+                lastLoadId: this.lastLoadId,
+                loadedFeed: mainFeedId
+            } as ManagerPost)
+        }
     }
 
 }
@@ -64,3 +143,7 @@ let feedsManager = new FeedsManager(undefined!)
 window.addEventListener("logged", () => (feedsManager = new FeedsManager(getWebSocket())).init())
 
 export { feedsManager }
+
+type ManagerPost = { lastLoadId: number, loadedFeed: number } & Post
+type FeedId = number | undefined
+const mainFeedId = -1
--- src/realtime/protocol/v1/packets/server/WSPSFeedPostCreated.ts
+++ src/realtime/protocol/v1/packets/server/WSPSFeedPostCreated.ts
@@ -4,9 +4,11 @@ import PacketIn from "../../../PacketIn"
 
 export default class WSPSFeedPostCreated implements PacketIn {
 
+    follow!: boolean
     post!: PostUpdate
 
     read(dataReader: DataReader): void {
+        this.follow = dataReader.readBoolean()
         this.post = JSON.parse(dataReader.readString())
     }
 
