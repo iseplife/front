d94580b101d10048f8e30143896337d087e15812
Paul <dev.warthy@gmail.com>
2022-03-16 19:28:31 +0100

    fix(group): student selector in group

--- src/components/Group/member/AddMember.tsx
+++ src/components/Group/member/AddMember.tsx
@@ -3,6 +3,7 @@ import StudentSelector from "../../Student/StudentSelector"
 import {useTranslation} from "react-i18next"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faPlus } from "@fortawesome/free-solid-svg-icons"
+import StudentPicker from "../../Student/StudentPicker"
 
 type AddMemberProps = {
     onAdd: (id: number) => void
@@ -10,10 +11,8 @@ type AddMemberProps = {
 const AddMember: React.FC<AddMemberProps> = ({onAdd}) => {
     const {t} = useTranslation("group")
 
-    const onSelect = useCallback((ids: number[])=> ids.forEach(onAdd), [onAdd])
-
+    const onSelect = useCallback((id, metq)=> onAdd(id), [onAdd])
     const [selected, setSelected] = useState(false)
-
     const selectorRef = useRef<HTMLButtonElement>(null)
 
     const input = selectorRef.current?.querySelector("input")
@@ -31,12 +30,17 @@ const AddMember: React.FC<AddMemberProps> = ({onAdd}) => {
     }, [input, selected])
     return (
         <div className={"flex justify-center mt-1 "+(selected && "mr-2")}>
-            <button ref={selectorRef} onClick={()=>setSelected(true)} className={"flex items-center mx-auto bg-indigo-400 text-base text-white cursor-pointer opacity-100 hover:opacity-80 duration-200 " + (selected ? "w-full py-1 px-2.5 rounded-lg h-10" : "w-8 h-8 px-2 py-2 rounded-full")}>
-                <FontAwesomeIcon
-                    icon={faPlus}
-                    className="w-4 h-4 block flex-shrink-0"
-                ></FontAwesomeIcon>
-                <StudentSelector onChange={onSelect} className="w-full ml-2 opacity-40 rounded-lg overflow-hidden" placeholder={t("add_member")} />
+            <button
+                ref={selectorRef}
+                onClick={()=>setSelected(true)}
+                className={"flex items-center mx-auto bg-indigo-400 text-base text-white cursor-pointer opacity-100 hover:opacity-80 duration-200 " + (selected ? "w-full py-1 px-2.5 rounded-lg h-10" : "w-8 h-8 px-2 py-2 rounded-full")}
+            >
+                <FontAwesomeIcon icon={faPlus} className="w-4 h-4 block flex-shrink-0"/>
+                <StudentPicker
+                    className="w-full ml-2 opacity-40 rounded-lg overflow-hidden"
+                    placeholder={t("add_member")}
+                    onChange={onSelect}
+                />
             </button>
         </div>
     )
