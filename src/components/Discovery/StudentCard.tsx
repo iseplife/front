fb57375b6e86d879456f58f5f475c8e40dc3e2bb
Léo MELKI <melkileo@gmail.com>
2022-03-24 17:32:08 +0100

    fix(discovery): text readability on dark image

--- src/components/Discovery/StudentCard.tsx
+++ src/components/Discovery/StudentCard.tsx
@@ -20,10 +20,10 @@ const StudentCard: React.FC<StudentCardProps> = React.memo(({id, fullname, pictu
             backgroundPosition: "center",
         }}
     >
-        <div className="absolute right-2.5 top-2.5 bg-white/30 rounded-lg grid place-items-center px-2 py-0.5 text-neutral-600 font-normal text-lg">
+        <div className="absolute right-2.5 top-2.5 bg-white/50 rounded-lg grid place-items-center px-2 py-0.5 text-neutral-700 font-normal text-lg">
             {promo}
         </div>
-        <div className="bg-white/40 rounded-xl backdrop-blur grid place-items-center px-2 py-2 leading-6 mb-3 mt-auto w-full text-neutral-800 font-semibold text-lg">
+        <div className="bg-white/50 rounded-xl backdrop-blur grid place-items-center px-2 py-2 leading-6 mb-3 mt-auto w-full text-neutral-900 font-semibold text-lg">
             <div className="line-clamp-2 text-ellipsis" title={fullname}>
                 {fullname}
             </div>
