0a690fe742375da16d7cdb5719c786a019785cd9
leomelki <melkileo@gmail.com>
2022-05-30 00:09:41 +0200

    feat(first-follow): added translations

--- src/components/FirstFollow/index.tsx
+++ src/components/FirstFollow/index.tsx
@@ -9,6 +9,7 @@ import { AppContext } from "../../context/app/context"
 import { didFirstFollow } from "../../data/student"
 import { AppActionType } from "../../context/app/action"
 import { useHistory } from "react-router-dom"
+import { useTranslation } from "react-i18next"
 
 const REQUIRED_FOLLOWING = 3
 
@@ -16,6 +17,8 @@ const FirstFollow: React.FC = () => {
     const [clubs, setClubs] = useState<ClubPreview[]>([])
     const [error, setError] = useState(false)
     const loading = useMemo(() => !clubs.length && !error, [clubs, error])
+
+    const { t } = useTranslation("first-follow")
 
     const history = useHistory()
 
@@ -48,15 +51,19 @@ const FirstFollow: React.FC = () => {
         <div className="fixed top-0 left-0 bg-black/40 backdrop-blur-sm w-screen h-screen z-[9999] flex flex-col box-border sm:p-3 ">
             <div className="w-full text-white text-center mt-5 sm:mt-10 hidden sm:block">
                 <div className="text-5xl font-bold">
-                    Suivez des associations
+                    { t("follow_clubs")}
                 </div>
                 <div className="text-xl font-medium text-white/70 mt-3">
-                    Vous devez suivre les associations pour voir leurs posts dans votre feed.
+                    { t("why_follow")}
                 </div>
             </div>
             <div className="py-2 px-4 text-black border-b bg-neutral-100 sm:hidden">
-                <div className="text-2xl font-bold">Suivez des associations !</div>
-                <div className="text-lg text-neutral-900">Vous verrez leurs posts dans votre feed</div>
+                <div className="text-2xl font-bold">
+                    { t("follow_clubs")}
+                </div>
+                <div className="text-lg text-neutral-900">
+                    { t("why_follow_small")}
+                </div>
             </div>
             <div className="relative sm:rounded-xl overflow-hidden bg-neutral-100 w-full sm:w-auto  m-auto sm:my-14 h-full max-w-full">
                 <div className="overflow-auto h-full p-4 sm:p-8 ">
@@ -121,11 +128,11 @@ const FirstFollow: React.FC = () => {
                 <div className="absolute bottom-0 left-0 w-full text-neutral-500 bg-neutral-300/30 py-3 text-center font-semibold text-lg backdrop-blur-md">
                     {
                         followed.length < REQUIRED_FOLLOWING ? 
-                            <>Suivez encore au moins {REQUIRED_FOLLOWING - followed.length} assos</>
+                            t("follow_needed", {clubs: REQUIRED_FOLLOWING - followed.length})
                             :
                             <div className="flex">
                                 <button onClick={didFollow} className="ml-auto mr-4 rounded-full px h-10 font-bold cursor-pointer select-none text-base grid place-items-center bg-indigo-400 hover:bg-opacity-90 px-5 text-white">
-                                    Suivant
+                                    { t("next") }
                                 </button>
                             </div>
                     }
