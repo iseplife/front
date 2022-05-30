bc75e2b2f3e7a9652aee8d67170c13e134109c20
leomelki <melkileo@gmail.com>
2022-05-30 19:33:37 +0200

    feat(feed-name): add feed name in context

--- src/components/Post/context/PostEventTag.tsx
+++ src/components/Post/context/PostEventTag.tsx
@@ -1,26 +1,24 @@
 import React from "react"
 import {useTranslation} from "react-i18next"
 import {Link} from "react-router-dom"
-import {useLiveQuery} from "dexie-react-hooks"
-import {eventsManager} from "../../../datamanager/EventsManager"
+import { FeedContext } from "../../../data/feed/types"
 
 type PostEventTagProps = {
-    id: number
+    context: FeedContext
 }
-export const PostEventTag: React.FC<PostEventTagProps> = ({id}) => {
+export const PostEventTag: React.FC<PostEventTagProps> = ({context}) => {
     const {t} = useTranslation("post")
-    const event = useLiveQuery(async () => await eventsManager.getEventByEventFeedId(id), [id])
 
-    return event ? (
-        <Link to={`/event/${event.id}`} className="min-w-0">
+    return context ? (
+        <Link to={`/event/${context.id}`} className="min-w-0">
             <div
                 className="flex text-sm rounded px-2 py-0.5 font-medium hover:shadow-sm transition-shadow items items-center bg-red-300"
-                title={t("post:posted_in_event", {event: event.title})}
+                title={t("post:posted_in_event", {event: context.name})}
             >
                 <div className="bg-neutral-50 w-4 h-4 shadow-sm rounded-sm mr-1.5 overflow-hidden ">
                     <div className="bg-red-500 w-4 h-[0.32rem]"/>
                 </div>
-                <div className="text-white text-ellipsis whitespace-nowrap overflow-hidden">{event.title}</div>
+                <div className="text-white text-ellipsis whitespace-nowrap overflow-hidden">{context.name}</div>
             </div>
         </Link>
     ): null
