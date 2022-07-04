df9d14059e1dde404cd6158dfce4c57ca3a2d212
leomelki <melkileo@gmail.com>
2022-07-04 18:55:27 +0200

    fix(gallery-loading): don't request club galleries if no id

--- src/components/Club/ClubDescription/ClubPresentation.tsx
+++ src/components/Club/ClubDescription/ClubPresentation.tsx
@@ -1,19 +1,15 @@
-import React, { useCallback, useContext, useState } from "react"
-import {Collapse} from "antd"
+import React, { useCallback, useContext } from "react"
 import About from "../About"
 import {useTranslation} from "react-i18next"
 import GalleriesPreview from "../../Gallery/GalleriesPreview"
-import {faChevronRight} from "@fortawesome/free-solid-svg-icons"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { Link } from "react-router-dom"
 import { ClubContext } from "../../../context/club/context"
 import { getClubGalleries } from "../../../data/club"
-
-const {Panel} = Collapse
 
 const ClubPresentation: React.FC = () => {
     const {t} = useTranslation(["club", "gallery"])
     const { club: { id } } = useContext(ClubContext)
+
+    const clubGalleriesCallback = useCallback((page?: number) => getClubGalleries(id, page), [getClubGalleries, id])
 
     return (
         <div key="desktop-display" className="w-full">
@@ -21,7 +17,7 @@ const ClubPresentation: React.FC = () => {
                 <span className="text-neutral-900 font-semibold text-base">{t("about")}</span>
                 <About/>
             </div>
-            <GalleriesPreview getGalleriesCallback={(page) => getClubGalleries(id, page)} />
+            <GalleriesPreview loading={!id} getGalleriesCallback={clubGalleriesCallback} />
         </div>
 
     )
