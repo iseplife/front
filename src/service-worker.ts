f81e58c86dae57cd78df091a7daa75396bc383e8
leomelki <melkileo@gmail.com>
2022-07-13 03:44:59 +0200

    feat(workbox): reduce cache expiration for alpha

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -2,6 +2,7 @@ import { precacheAndRoute } from "workbox-precaching"
 import { registerRoute, Route } from "workbox-routing"
 import { StaleWhileRevalidate } from "workbox-strategies"
 import { initPushWorker } from "./push-worker"
+import { ExpirationPlugin } from "workbox-expiration"
 
 declare const self: Window & typeof globalThis & ServiceWorkerGlobalScope
 export {}
@@ -15,10 +16,18 @@ self.addEventListener("install", function (event) {
 const registerCacheFirstRouteUsing = (
     destination: RequestDestination,
     cacheName: string,
+    maxEntries: number,
+    maxAgeMins: number,
 ): Route => registerRoute(
     ({ request }) => request.destination === destination,
     new StaleWhileRevalidate({
         cacheName: cacheName,
+        plugins: [
+            new ExpirationPlugin({
+                maxEntries,
+                maxAgeSeconds: maxAgeMins * 60, 
+            })
+        ]
     })
 )
 
@@ -28,10 +37,10 @@ const CACHE_STYLES_NAME = `${CACHE_PREFIX}-styles`
 const CACHE_DOCUMENTS_NAME = `${CACHE_PREFIX}-documents`
 const CACHE_FONTS_NAME = `${CACHE_PREFIX}-fonts`
 const CACHE_IMAGES_NAME = `${CACHE_PREFIX}-images`
-registerCacheFirstRouteUsing("style", CACHE_STYLES_NAME)
-registerCacheFirstRouteUsing("script", CACHE_SCRIPT_NAME)
-registerCacheFirstRouteUsing("document", CACHE_DOCUMENTS_NAME)
-registerCacheFirstRouteUsing("font", CACHE_FONTS_NAME)
-registerCacheFirstRouteUsing("image", CACHE_IMAGES_NAME)
+registerCacheFirstRouteUsing("style", CACHE_STYLES_NAME, 60, 7)
+registerCacheFirstRouteUsing("script", CACHE_SCRIPT_NAME, 10, 7)
+registerCacheFirstRouteUsing("document", CACHE_DOCUMENTS_NAME, 5, 60 * 24 * 5)
+registerCacheFirstRouteUsing("font", CACHE_FONTS_NAME, 10, 60 * 24 * 30)
+registerCacheFirstRouteUsing("image", CACHE_IMAGES_NAME, 100, 60 * 24 * 10)
 
 initPushWorker()
