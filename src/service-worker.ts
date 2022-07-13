31a1357c8996a9af70c05611905297a2d6b253a9
leomelki <melkileo@gmail.com>
2022-07-13 05:44:38 +0200

    fix(workbox): reduce cache expiration for documents (html...)

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -18,8 +18,9 @@ const registerCacheFirstRouteUsing = (
     cacheName: string,
     maxEntries: number,
     maxAgeMins: number,
+    excludePaths: string[] = []
 ): Route => registerRoute(
-    ({ request }) => request.destination === destination,
+    ({ request }) => request.destination === destination && !excludePaths.find(path => request.url.includes(path)),
     new StaleWhileRevalidate({
         cacheName: cacheName,
         plugins: [
@@ -39,8 +40,8 @@ const CACHE_FONTS_NAME = `${CACHE_PREFIX}-fonts`
 const CACHE_IMAGES_NAME = `${CACHE_PREFIX}-images`
 registerCacheFirstRouteUsing("style", CACHE_STYLES_NAME, 60, 7)
 registerCacheFirstRouteUsing("script", CACHE_SCRIPT_NAME, 10, 7)
-registerCacheFirstRouteUsing("document", CACHE_DOCUMENTS_NAME, 5, 60 * 24 * 5)
+registerCacheFirstRouteUsing("document", CACHE_DOCUMENTS_NAME, 5, 7)
 registerCacheFirstRouteUsing("font", CACHE_FONTS_NAME, 10, 60 * 24 * 30)
-registerCacheFirstRouteUsing("image", CACHE_IMAGES_NAME, 100, 60 * 24 * 10)
+registerCacheFirstRouteUsing("image", CACHE_IMAGES_NAME, 450, 60 * 24 * 12, ["mapbox/"])
 
 initPushWorker()
