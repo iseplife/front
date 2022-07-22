4ad233ab808347a24a040dd6367a2f58b6646bf3
leomelki <melkileo@gmail.com>
2022-07-22 17:00:01 +0200

    fix(student): fix student's clubs cards

--- src/components/Student/StudentClubs.tsx
+++ src/components/Student/StudentClubs.tsx
@@ -31,9 +31,9 @@ const StudentClubs: React.FC<{student?: StudentPreview}> = ({ student }) => {
                     >
                         <FontAwesomeIcon className="absolute left-2 top-2 text-white/80 drop-shadow-sm text-xl" icon={ClubRoleIcon[club.role]}/>
 
-                        <div className="bg-white/40 rounded-xl backdrop-blur grid place-items-center px-3 py-1 mb-3 mt-auto w-full text-neutral-800 font-semibold text-2xl">
+                        <div className="text-center bg-white/40 rounded-xl backdrop-blur grid place-items-center px-3 py-1 mb-3 mt-auto w-full text-neutral-900 font-semibold text-2xl">
                             {club.club.name}
-                            <span className="text-sm font-bold text-neutral-600 text-center">
+                            <span className="text-sm font-bold text-neutral-700 text-center">
                                 {club.position}
                             </span>
                         </div>
