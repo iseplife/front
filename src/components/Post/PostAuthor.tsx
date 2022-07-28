70990dc737f21a75d2087ec013c3e948c7744d21
leomelki <melkileo@gmail.com>
2022-07-28 23:40:01 +0200

    fix(post): fix post displayed as planned and not updating

--- src/components/Post/PostAuthor.tsx
+++ src/components/Post/PostAuthor.tsx
@@ -1,9 +1,9 @@
-import React, {useCallback, useEffect, useMemo, useState} from "react"
+import React, {useCallback, useEffect, useState} from "react"
 import {Author} from "../../data/request.type"
 import {Link} from "react-router-dom"
 import StudentAvatar from "../Student/StudentAvatar"
 import {AvatarSizes} from "../../constants/MediaSizes"
-import {format, isFuture} from "date-fns"
+import {format, isFuture, addSeconds} from "date-fns"
 import {useTranslation} from "react-i18next"
 import {formatDateWithTimer} from "../../util"
 import {Post} from "../../data/post/types"
@@ -14,9 +14,27 @@ type PostAuthorProps = {
     className?: string
 }
 const PostAuthor: React.FC<PostAuthorProps> = ({author, publicationDate, className = ""}) => {
-    const {t} = useTranslation( "post")
+    const {t} = useTranslation("post")
     const [formattedDate, setFormattedDate] = useState<string>("")
     useEffect(() => formatDateWithTimer(publicationDate, t, setFormattedDate), [publicationDate])
+
+    const checkFuturePost = (publicationDate: Date) => isFuture(addSeconds(publicationDate, -5)) // -5s for potential clocks diff
+
+    const [futurePost, setFuturePost] = useState(checkFuturePost(publicationDate))
+
+    useEffect(() => {
+        setFuturePost(checkFuturePost(publicationDate))
+        if (checkFuturePost(publicationDate)) {
+            const id = setInterval(() => {
+                const future = checkFuturePost(publicationDate)
+                if (!future) {
+                    setFuturePost(future)
+                    clearInterval(id)
+                }
+            }, 30_000)
+            return () => clearInterval(id)
+        }
+    }, [publicationDate])
 
     const AvatarWrapper = useCallback<React.FC<{children: React.ReactNode}>>(({children}) => {
         switch (author.authorType) {
@@ -43,7 +61,7 @@ const PostAuthor: React.FC<PostAuthorProps> = ({author, publicationDate, classNa
                 <div className="items-center ml-2">
                     <div className="font-bold -mb-0.5 -mt-0.5 group-hover:underline">{author.name}</div>
                     <div className="text-xs whitespace-nowrap">
-                        {isFuture(publicationDate) ?
+                        {futurePost ?
                             `${t("planned_for")} ${format(new Date(publicationDate), "dd/MM/yy HH:mm")}` :
                             formattedDate
                         }
