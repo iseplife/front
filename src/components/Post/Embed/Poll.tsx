fc1a58e7776e45ba6d83f9de171379c53f7578a0
leomelki <melkileo@gmail.com>
2022-07-30 02:43:04 +0200

    feat(poll): add total votes number

--- src/components/Post/Embed/Poll.tsx
+++ src/components/Post/Embed/Poll.tsx
@@ -3,7 +3,7 @@ import {useTranslation} from "react-i18next"
 import {Poll as PollType} from "../../../data/poll/types"
 import PollChoice from "./PollChoice"
 import {isPast} from "date-fns"
-import {message} from "antd"
+import {Divider, message} from "antd"
 import {addVote, getPollVotes, removeVote} from "../../../data/poll"
 import {_formatDistance} from "../../../util"
 
@@ -68,9 +68,15 @@ const Poll: React.FC<PollProps> = ({data}) => {
                 ))}
             </div>
 
-            <span className="text-gray-500 text-xs lowercase">
-                {poll.multiple && (t("allow_multiple") + ", ")} {poll.anonymous && t("anonymous_poll")}
-            </span>
+            <div className="flex text-neutral-600 text-sm items-center">
+                <div>{total} {t("votes")}</div>
+                {(poll.multiple || poll.anonymous) && <>
+                    <Divider type="vertical" className="mt-1" />
+                    <span className="lowercase">
+                        {poll.multiple && (t("allow_multiple") + ", ")} {poll.anonymous && t("anonymous_poll")}
+                    </span>
+                </>}
+            </div>
         </div>
     )
 }
