8d550f73760596e4e10dd8dd696cac63a38329a5
leomelki <melkileo@gmail.com>
2022-08-01 14:35:18 +0200

    feat(version): add commit version

--- craco.config.js
+++ craco.config.js
@@ -1,4 +1,8 @@
 const CracoEsbuildPlugin = require("craco-esbuild")
+const commitHash = require('child_process').execSync('git rev-parse --short HEAD').toString()
+
+process.env.REACT_APP_COMMIT = commitHash
+console.log("Hash set to", commitHash)
 
 module.exports = {
     plugins: [
