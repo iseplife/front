a2822d40ca3a2e2cea828c596b430d4c270e260d
leomelki <melkileo@gmail.com>
2022-08-01 15:51:18 +0200

    fix(version): get commit version without git

--- craco.config.js
+++ craco.config.js
@@ -1,5 +1,9 @@
 const CracoEsbuildPlugin = require("craco-esbuild")
-const commitHash = require('child_process').execSync('git rev-parse --short HEAD').toString()
+const fs = require('fs')
+
+const file = fs.readFileSync(".git/HEAD").toString()
+const hash = !file.includes("/") ? file.split("\n")[0] : fs.readFileSync(`.git/${file.split("\n")[0].substring(5)}`)
+const commitHash = hash.toString().trim().substring(0, 6)
 
 process.env.REACT_APP_COMMIT = commitHash
 console.log("Hash set to", commitHash)
