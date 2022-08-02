4af9d2b520289a894606ac8ce5b0cea9a2c96172
leomelki <melkileo@gmail.com>
2022-08-02 05:06:46 +0200

    fix(group-members): fix group members look

--- src/components/Group/MemberList.tsx
+++ src/components/Group/MemberList.tsx
@@ -4,6 +4,7 @@ import {GroupMember} from "../../data/group/types"
 import {useTranslation} from "react-i18next"
 import StudentAvatar from "../Student/StudentAvatar"
 import { AppContext } from "../../context/app/context"
+import { Link } from "react-router-dom"
 
 export const MEMBER_PREVIEW_COUNT = 5
 
@@ -17,22 +18,20 @@ const MemberList: React.FC<MemberListProps> = ({members, className, actions, act
     const {state: {user}} = useContext(AppContext)
     return (
         <div>
-            <div className={`${className} flex flex-col overflow-y-auto`} style={{maxHeight: 400}}>
+            <div className={`${className} flex flex-col overflow-y-auto overflow-x-hidden w-full`} style={{maxHeight: 400}}>
                 {members.map(({id, student}) => (
-                    <span key={student.id} className="my-1 flex">
+                    <Link to={`/student/${student.id}`} key={student.id} className="my-1 flex w-full mr-2 text-gray-500 hover:text-gray-600 hover:bg-black hover:bg-opacity-5 transition-colors rounded-lg p-2 items-center">
                         <StudentAvatar
                             id={student.id}
                             name={student.firstName + " " + student.lastName}
                             picture={student.picture}
                             pictureSize={AvatarSizes.THUMBNAIL}
                             size="small"
-                            className="mr-2 text-gray-500 hover:text-gray-600 hover:bg-black hover:bg-opacity-5 transition-colors rounded-lg p-2 w-full"
-                            showPreview
-                        >
-                            <div className="ml-2 flex-shrink-0">{student.firstName + " " + student.lastName}</div>
-                            {members.length >= actionsTrigger && student.id != user.id && <div className="mr-0 ml-auto">{actions(id)}</div>}
-                        </StudentAvatar>
-                    </span>
+                            className="flex-shrink-0"
+                        />
+                        <div className="ml-2 overflow-hidden text-ellipsis whitespace-nowrap">{student.firstName + " " + student.lastName}</div>
+                        {members.length >= actionsTrigger && student.id != user.id && <div className="mr-0 ml-auto">{actions(id)}</div>}
+                    </Link>
                 ))}
             </div>
         </div>
