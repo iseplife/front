4b21e6a290d7082de92b7fb6016633f448f4056d
leomelki <melkileo@gmail.com>
2022-08-25 21:05:43 +0200

    feat(ionic): add ionic WIP

--- src/push-worker.ts
+++ src/push-worker.ts
@@ -1,45 +1,52 @@
 /// <reference lib="WebWorker" />
 
-import { AvatarSizes } from "./constants/MediaSizes"
-import { apiURI } from "./data/http.constants"
-import { mediaPath } from "./util"
+import { onBackgroundMessage, getMessaging } from "firebase/messaging/sw"
+import { firebaseApp } from "./data/firebase"
 
 declare const self: ServiceWorkerGlobalScope & typeof globalThis
 
-export const initPushWorker = () => {
-    self.addEventListener("push", async function (event) {
-        if (!event.data)
-            return
-        
-        const json = JSON.parse(event.data.text())
-        console.log("[Service Worker] Push received:", json)
+const firebaseMessaging = getMessaging(firebaseApp)
 
-        switch (json.type) {
-            case "register":
-                fetch(`${apiURI}/webpush/register/validate`, {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json"
-                    },
-                    body: JSON.stringify({
-                        key: json.key,
-                    })
-                })
-                break
-            case "notification":
-                event.waitUntil(self.registration.showNotification("IsepLife", {
-                    body: json.text,
-                    icon: mediaPath(json.icon, AvatarSizes.THUMBNAIL),
-                    silent: false,
-                    data: {
-                        url: `/${json.link}`
-                    }
-                }))
-                break
-            default:
-                break
-        }
-    })
+onBackgroundMessage(firebaseMessaging, (payload) => {
+    console.log("Received background message ", payload)
+})
+
+console.log("load firebase")
+
+export const initPushWorker = () => {
+    // self.addEventListener("push", async function (event) {
+    //     if (!event.data)
+    //         return
+        
+    //     const json = JSON.parse(event.data.text())
+    //     console.log("[Service Worker] Push received:", json)
+
+    //     switch (json.type) {
+    //         case "register":
+    //             fetch(`${apiURI}/webpush/register/validate`, {
+    //                 method: "POST",
+    //                 headers: {
+    //                     "Content-Type": "application/json"
+    //                 },
+    //                 body: JSON.stringify({
+    //                     key: json.key,
+    //                 })
+    //             })
+    //             break
+    //         case "notification":
+    //             event.waitUntil(self.registration.showNotification("IsepLife", {
+    //                 body: json.text,
+    //                 icon: mediaPath(json.icon, AvatarSizes.THUMBNAIL),
+    //                 silent: false,
+    //                 data: {
+    //                     url: `/${json.link}`
+    //                 }
+    //             }))
+    //             break
+    //         default:
+    //             break
+    //     }
+    // })
     
     self.addEventListener("notificationclick", function(event) {
         event.notification.close()
