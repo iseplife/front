955efc123a2f1ea5ba801dc452e9a6cf96eb4b5b
leomelki <melkileo@gmail.com>
2022-08-30 01:29:46 +0200

    fix(logout): fix logout mobile

--- src/pages/security/Logout.tsx
+++ src/pages/security/Logout.tsx
@@ -3,14 +3,18 @@ import {Redirect} from "react-router-dom"
 import {AppContext} from "../../context/app/context"
 import {AppActionType} from "../../context/app/action"
 import {logout} from "../../data/security"
+import { isWeb } from "../../data/app"
 
 
 const Logout: React.FC = () => {
     const {dispatch} = useContext(AppContext)
     useEffect(() => {
-        logout().then(() => {
-            dispatch({type: AppActionType.SET_LOGGED_OUT})
-        })
+        if(isWeb)
+            logout().then(() => {
+                dispatch({type: AppActionType.SET_LOGGED_OUT})
+            })
+        else
+            localStorage.removeItem("refresh")
     }, [])
 
     return <Redirect to="/login" />
