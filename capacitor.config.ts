2214339afb82fed07a1cd309d03c4ebffb7bdf52
leomelki <melkileo@gmail.com>
2022-08-30 03:55:42 +0200

    refactor(app): renamed to IsepLife

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'fr.iseplive.iseplife',
-  appName: 'iseplife',
+  appName: 'IsepLife',
   webDir: 'build',
   bundledWebRuntime: false,
   "plugins": {
