31417de73a477aab22a598785a5b5e4ca63cc759
leomelki <melkileo@gmail.com>
2022-08-31 09:25:14 -0700

    fix(typo): remove ionic breaking css changes

--- src/index.tsx
+++ src/index.tsx
@@ -40,10 +40,6 @@ import NotificationClickHandler from "./components/Notification/NotificationClic
 /* Core CSS required for Ionic components to work properly */
 import "@ionic/react/css/core.css"
 
-/* Basic CSS for apps built with Ionic */
-import "@ionic/react/css/normalize.css"
-import "@ionic/react/css/structure.css"
-import "@ionic/react/css/typography.css"
 import { IonApp, setupIonicReact } from "@ionic/react"
 
 setupIonicReact()
