bb87a68af3d3413f006d233dc35ac9a552fc00ae
leomelki <melkileo@gmail.com>
2022-09-05 02:56:57 +0200

    feat(yearbook): fix loading

--- src/components/Discovery/YearBook.tsx
+++ src/components/Discovery/YearBook.tsx
@@ -140,6 +140,7 @@ const YearBook: React.FC = () => {
                 watch="DOWN"
                 callback={getNextStudents}
                 className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 place-items-center mt-10 gap-5"
+                loaderClassName="bottom-10"
                 loadingComponent={<></>}
             >
                 {filteredStudent.length == 0 && !loading ?
