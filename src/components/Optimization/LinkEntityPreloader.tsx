01ac82b1b1fe3bbe7d8e6032484c5d979e1fc6de
leomelki <melkileo@gmail.com>
2022-09-05 03:18:57 +0200

    fix(home): fix event on md & lg

--- src/components/Optimization/LinkEntityPreloader.tsx
+++ src/components/Optimization/LinkEntityPreloader.tsx
@@ -9,14 +9,15 @@ import { entityPreloader } from "./EntityPreloader"
 interface LinkEntityPreloaderProps {
     preview: Author | StudentPreview | ClubPreview | EventPreview | SearchItem | undefined
     children: ReactElement
+    className?: string
     noWidth?: boolean
 }
 
-const LinkEntityPreloader: React.FC<LinkEntityPreloaderProps> = ({preview, children, noWidth}) => {
+const LinkEntityPreloader: React.FC<LinkEntityPreloaderProps> = ({preview, className, children, noWidth}) => {
     const onClick = useCallback(() =>
         preview && entityPreloader.set(preview.id, preview!)
     , [preview])
-    return <div className={noWidth ? "" : "w-full"} onClick={onClick}>
+    return <div className={`${noWidth || "w-full"} ${className}`} onClick={onClick}>
         {children}
     </div>
 }
