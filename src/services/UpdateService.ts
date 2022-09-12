5ab11bddf11e5d6ea7e7f895bb6125ed73ff9abb
leomelki <melkileo@gmail.com>
2022-09-12 15:23:19 +0200

    fix(update): stop updating auto

--- src/services/UpdateService.ts
+++ src/services/UpdateService.ts
@@ -29,45 +29,45 @@ export default class UpdateService {
                     })
             }
         })
-        CapacitorUpdater.addListener("updateAvailable", async (version) => {
-            if(updateShown)
-                return
-            updateShown = true
-            console.log(version)
-            if (Date.now() - pageOpenned < 2_500)
-                setTimeout(() => 
-                    CapacitorUpdater.set({
-                        version: version.version ?? (version as any ).newVersion
-                    })
-                , 200)
-            else if(isPlatform("android")){
-                const last = localStorage.getItem("lastUpdates")
-                const { versions } = await CapacitorUpdater.list()
-                localStorage.setItem("lastUpdates", JSON.stringify(versions))
-                if(last) {
-                    const lastVersions = JSON.parse(last) as string[]
-                    const newVersion = versions.find(version => !lastVersions.includes(version))
-                    console.debug("[Updater] Old versions were", last, "and now", versions)
-                    console.debug("[Updater] Selecting", newVersion, "for update")
-                    if(newVersion){
-                        message.info({
-                            content: t("update_available").toString(),
-                            duration: 10,
-                            onClick: () => CapacitorUpdater.set({
-                                version: newVersion,
-                                versionName: version.version ?? (version as any ).newVersion
-                            })
-                        })
-                    }
-                }
-            }
-        })
+        // CapacitorUpdater.addListener("updateAvailable", async (version) => {
+        //     if(updateShown)
+        //         return
+        //     updateShown = true
+        //     console.log(version)
+        //     if (Date.now() - pageOpenned < 2_500)
+        //         setTimeout(() => 
+        //             CapacitorUpdater.set({
+        //                 version: version.version ?? (version as any ).newVersion
+        //             })
+        //         , 200)
+        //     else if(isPlatform("android")){
+        //         const last = localStorage.getItem("lastUpdates")
+        //         const { versions } = await CapacitorUpdater.list()
+        //         localStorage.setItem("lastUpdates", JSON.stringify(versions))
+        //         if(last) {
+        //             const lastVersions = JSON.parse(last) as string[]
+        //             const newVersion = versions.find(version => !lastVersions.includes(version))
+        //             console.debug("[Updater] Old versions were", last, "and now", versions)
+        //             console.debug("[Updater] Selecting", newVersion, "for update")
+        //             if(newVersion){
+        //                 message.info({
+        //                     content: t("update_available").toString(),
+        //                     duration: 10,
+        //                     onClick: () => CapacitorUpdater.set({
+        //                         version: newVersion,
+        //                         versionName: version.version ?? (version as any ).newVersion
+        //                     })
+        //                 })
+        //             }
+        //         }
+        //     }
+        // })
         
-        const last = localStorage.getItem("lastUpdates")
-        if(!last)
-            CapacitorUpdater.list().then(({versions}) => 
-                localStorage.setItem("lastUpdates", JSON.stringify(versions))
-            )
+        // const last = localStorage.getItem("lastUpdates")
+        // if(!last)
+        //     CapacitorUpdater.list().then(({versions}) => 
+        //         localStorage.setItem("lastUpdates", JSON.stringify(versions))
+        //     )
         
     }
 }
