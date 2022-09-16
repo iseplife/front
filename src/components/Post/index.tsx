2ed04e58362c4ee65529249751c747a659e608a4
leomelki <melkileo@gmail.com>
2022-09-16 23:29:03 +0200

    fix(post): limit to 1000

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -180,7 +180,7 @@ const Post: React.FC<PostProps> = ({data, feedId, isEdited, forceShowComments = 
                     </div>
                 </div>
                 <div>
-                    <CustomText descLengthThrottle={1400} description={data.description} />
+                    <CustomText descLengthThrottle={1000} description={data.description} />
                     {data.embed &&
                         <div className="mt-2">
                             <Embed embed={data.embed} post={data} selected={selected} />
