2a286f44fafa32444b75eea04c8df3f583e561ac
leomelki <melkileo@gmail.com>
2022-09-23 14:34:38 +0200

    feat(update): re enable updates

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -6,6 +6,10 @@ const config: CapacitorConfig = {
   webDir: 'build',
   bundledWebRuntime: false,
   "plugins": {
+    "CapacitorUpdater": {
+      "autoUpdate": true,
+      "autoUpdateUrl": "https://api.iseplife.fr/health/update"
+    },
     "PushNotifications": {
       "presentationOptions": ["badge", "sound", "alert"]
     }
