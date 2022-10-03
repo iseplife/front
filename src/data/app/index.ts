f900d645ff1f0824db772e9242b782a6891098e5
leomelki <melkileo@gmail.com>
2022-10-03 19:48:11 +0200

    fix(canvas): fix gallery save size & safari detection

--- src/data/app/index.ts
+++ src/data/app/index.ts
@@ -4,7 +4,7 @@ import {isWebKit as isWebKitFP} from "@fingerprintjs/fingerprintjs"
 export const isWeb = !isPlatform("cordova")
 export const isIosApp = isPlatform("ios")
 export const isAndroidApp = isPlatform("android")
-export const isWebKit = navigator.userAgent.includes("AppleWebKit") || isWebKitFP()
+export const isWebKit = !!navigator.userAgent.match(/Version\/[\d.]+.*Safari/) || isWebKitFP()
 
 console.debug("Platforms :", getPlatforms())
 console.log("Loaded on web :", isWeb)
