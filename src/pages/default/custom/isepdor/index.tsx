18f5edc2bdedda03253a4e42f1345b5b4888eced
leomelki <melkileo@gmail.com>
2023-03-23 14:58:43 +0100

    feat(ior): wip

---
+++ src/pages/default/custom/isepdor/index.tsx
@@ -0,0 +1,23 @@
+import React from "react"
+
+const IsepDor: React.FC = () => {
+    return <div className="mx-auto container px-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 sm:gap-10 mt-8">
+        <div className="flex flex-col p-4 rounded-lg bg-white relative shadow-sm aspect-square">
+
+        </div>
+        <div className="flex flex-col p-4 rounded-lg bg-white relative shadow-sm aspect-square">
+
+        </div>
+        <div className="flex flex-col p-4 rounded-lg bg-white relative shadow-sm aspect-square">
+
+        </div>
+        <div className="flex flex-col p-4 rounded-lg bg-white relative shadow-sm aspect-square">
+
+        </div>
+        <div className="flex flex-col p-4 rounded-lg bg-white relative shadow-sm aspect-square">
+
+        </div>
+    </div>
+}
+
+export default IsepDor
