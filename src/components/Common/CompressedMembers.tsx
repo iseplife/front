ff6f44a2b9f23dce09629337f8a443b470a58307
Clément Delestre <clementdelestre@gmail.com>
2023-10-17 11:58:40 -0700

    fix(group): fix compressed members list UI

--- src/components/Common/CompressedMembers.tsx
+++ src/components/Common/CompressedMembers.tsx
@@ -13,7 +13,7 @@ type CompressedMembersProps = {
 const CompressedMembers: React.FC<CompressedMembersProps> = ({ members, className, onClick }) => {
     return <div className={"flex " + className} onClick={onClick}> {
         members.slice(0, 9).map((student, index, array) =>
-            <div className="-mr-1.5 relative" style={{ zIndex: 100 - index }} title={`${student.firstName} ${student.lastName}`} key={index}>
+            <div className="-mr-1.5 relative" style={{ zIndex: 10 - index }} title={`${student.firstName} ${student.lastName}`} key={index}>
                 {array.length - 1 == index && array.length != members.length && <div className="rounded-full bg-black/[42%] absolute w-full h-full z-10 text-gray-300 items-center justify-center flex text-xs border-gray-100 border-2">
                     <div className="transform scale-50 flex">
                         <FontAwesomeIcon icon={faCircle} size="xs" className="mr-0.5"></FontAwesomeIcon>
