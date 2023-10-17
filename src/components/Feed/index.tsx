41dac0cfae590ac426343471b3aa8d56dfe773ea
Clément Delestre <clementdelestre@gmail.com>
2023-10-17 11:42:51 -0700

    feat(post): force opening the modal to write a post

--- src/components/Feed/index.tsx
+++ src/components/Feed/index.tsx
@@ -401,17 +401,8 @@ const Feed: React.FC<FeedProps> = ({loading, id, allowPublication, style, classN
             }
             {!noDivider && <Divider className="text-gray-700 text-lg" orientation="left">{t("posts")}</Divider>}
             {allowPublication && (
-                <BasicPostForm setText={setText} user={user} feed={id} onPost={onPostCreation}>
-                    <div className="grid grid-cols-5 sm:gap-2.5 items-center text-xl mt-1 -mb-2">
-                        <div
-                            onClick={() => setCompleteFormType(EmbedEnumType.RICH_LINK)}
-                            className="w-10 h-10 justify-center items-center rounded-full hover:bg-gray-100 transition-colors cursor-pointer group xsm"
-                        >
-                            <FontAwesomeIcon
-                                icon={faAlignLeft}
-                                className="text-gray-700 text-opacity-60 mx-1 group-hover:text-opacity-100 transition-colors"
-                            />
-                        </div>
+                <BasicPostForm setText={setText} user={user} feed={id} onPost={onPostCreation} onInputClicked={() => setCompleteFormType(EmbedEnumType.RICH_LINK)}>
+                    <div className="grid grid-cols-4 sm:gap-2.5 items-center text-xl mt-1 -mb-2">
                         <div
                             onClick={() => setCompleteFormType(EmbedEnumType.IMAGE)}
                             className="w-10 h-10 justify-center items-center rounded-full hover:bg-gray-100 transition-colors cursor-pointer group xsm"
