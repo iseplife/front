1d32af9282fcaf731f965b3a2e7c47038c7ff149
Léo MELKI <melkileo@gmail.com>
2024-09-27 14:13:58 +0200

    fix(dd): set as localhost only when loaded on web

--- src/util.ts
+++ src/util.ts
@@ -17,7 +17,6 @@ import { Media } from "@capacitor-community/media"
 import { Toast } from "@capacitor/toast"
 import { message } from "antd"
 
-
 const locales: { [id: string]: Locale } = {
     en: enUS,
     fr
@@ -79,13 +78,15 @@ export const _formatDistance = (date: Date | number, baseDate: Date | number, op
 
 
 export const isLocalhost = Boolean(
-    window.location.hostname === "localhost" ||
-    // [::1] is the IPv6 localhost address.
-    window.location.hostname === "[::1]" ||
-    // 127.0.0.0/8 are considered localhost for IPv4.
-    window.location.hostname.match(
-        /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
-    )
+    (
+        window.location.hostname === "localhost" ||
+        // [::1] is the IPv6 localhost address.
+        window.location.hostname === "[::1]" ||
+        // 127.0.0.0/8 are considered localhost for IPv4.
+        window.location.hostname.match(
+            /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+        )
+    ) && isWeb
 )
 
 export const handleRequestCancellation = (e: Error): void | Error => {
